coscene-io/honeybee

commit 871ecda39a86d3716950cfe5f69d7ecd9a87a348

Fix unchecked index access in app/components (#321)

app/components/PanelToolbar/index.tsx
@@ -93,10 +93,14 @@ function StandardMenuItems({
   );
 
   const close = useCallback(() => {
-    logEvent({
-      name: getEventNames().PANEL_REMOVE,
-      tags: { [getEventTags().PANEL_TYPE]: getPanelType() },
-    });
+    const name = getEventNames().PANEL_REMOVE;
+    const type = getEventTags().PANEL_TYPE;
+    if (name != undefined && type != undefined) {
+      logEvent({
+        name: name,
+        tags: { [type]: getPanelType() },
+      });
+    }
     actions.closePanel({
       tabId,
       root: mosaicActions.getRoot() as any,
@@ -110,12 +114,20 @@ function StandardMenuItems({
       if (!id || !type) {
         throw new Error("Trying to split unknown panel!");
       }
-      logEvent({
-        name: getEventNames().PANEL_SPLIT,
-        tags: { [getEventTags().PANEL_TYPE]: getPanelType() },
-      });
+      const name = getEventNames().PANEL_SPLIT;
+      const eventType = getEventTags().PANEL_TYPE;
+      if (name != undefined && eventType != undefined) {
+        logEvent({
+          name: name,
+          tags: { [eventType]: type },
+        });
+      }
 
       const config = savedProps[id];
+      if (!config) {
+        return;
+      }
+
       actions.splitPanel({
         id,
         tabId,
@@ -139,7 +151,14 @@ function StandardMenuItems({
         config: config as any,
         relatedConfigs,
       });
-      logEvent({ name: getEventNames().PANEL_SWAP, tags: { [getEventTags().PANEL_TYPE]: type } });
+      const name = getEventNames().PANEL_SWAP;
+      const eventType = getEventTags().PANEL_TYPE;
+      if (name != undefined && eventType != undefined) {
+        logEvent({
+          name: name,
+          tags: { [eventType]: type },
+        });
+      }
     },
     [actions, mosaicActions, mosaicWindowActions, tabId],
   );
@@ -329,9 +348,9 @@ export default React.memo<Props>(function PanelToolbar({
           {shareModal}
           <div
             className={cx(styles.panelToolbarContainer, {
-              [styles.floating]: floating,
-              [styles.floatingShow]: floating && isRendered,
-              [styles.hasChildren]: !!children,
+              [styles.floating!]: floating,
+              [styles.floatingShow!]: floating && isRendered,
+              [styles.hasChildren!]: !!children,
             })}
           >
             {(isRendered || !floating) && children}

app/components/AddPanelMenu/index.tsx
@@ -35,7 +35,12 @@ function AddPanelMenu(props: Props) {
   const onPanelSelect = useCallback(
     ({ type, config, relatedConfigs }: PanelSelection) => {
       dispatch(addPanel({ type, layout, config, relatedConfigs } as AddPanelPayload));
-      logEvent({ name: getEventNames().PANEL_ADD, tags: { [getEventTags().PANEL_TYPE]: type } });
+
+      const name = getEventNames().PANEL_ADD;
+      const panelType = getEventTags().PANEL_TYPE;
+      if (name != undefined && panelType != undefined) {
+        logEvent({ name: name, tags: { [panelType]: type } });
+      }
     },
     [dispatch, layout],
   );

app/components/PlaybackControls/index.tsx
@@ -180,7 +180,7 @@ export const UnconnectedPlaybackControls = memo<PlaybackControlProps>(
           <Button
             onClick={() => jumpSeek(DIRECTION.BACKWARD, { seek, player: playerState.current })}
             style={{ borderRadius: "4px 0px 0px 4px", marginLeft: "16px", marginRight: "1px" }}
-            className={cx([styles.seekBtn, { [styles.inactive]: !activeData }])}
+            className={cx([styles.seekBtn, { [styles.inactive!]: !activeData }])}
           >
             <Icon medium tooltip="Seek backward">
               <SkipPreviousOutlineIcon />
@@ -189,7 +189,7 @@ export const UnconnectedPlaybackControls = memo<PlaybackControlProps>(
           <Button
             onClick={() => jumpSeek(DIRECTION.FORWARD, { seek, player: playerState.current })}
             style={{ borderRadius: "0px 4px 4px 0px" }}
-            className={cx([styles.seekBtn, { [styles.inactive]: !activeData }])}
+            className={cx([styles.seekBtn, { [styles.inactive!]: !activeData }])}
           >
             <Icon medium tooltip="Seek forward">
               <SkipNextOutlineIcon />

app/components/Autocomplete.tsx
@@ -28,7 +28,7 @@ function measureText(text: string): number {
   return textWidth(text, { family: fontFamily, size: fontSize }) + 3;
 }
 
-const rowHeight = parseInt(styles.rowHeight);
+const rowHeight = parseInt(styles.rowHeight ?? "24");
 
 // <Autocomplete> is a Webviz-specific autocomplete with support for things like multiple
 // autocompletes that seamlessly transition into each other, e.g. when building more complex
@@ -283,8 +283,8 @@ export default class Autocomplete extends PureComponent<AutocompleteProps, Autoc
               data-highlighted={isHighlighted}
               data-test-auto-item
               className={cx(styles.autocompleteItem, {
-                [styles.highlighted]: isHighlighted,
-                [styles.selected]:
+                [styles.highlighted!]: isHighlighted,
+                [styles.selected!]:
                   selectedItemValue != undefined && itemValue === selectedItemValue,
               })}
             >
@@ -297,8 +297,8 @@ export default class Autocomplete extends PureComponent<AutocompleteProps, Autoc
         value={value || ""}
         inputProps={{
           className: cx(styles.input, {
-            [styles.inputError]: hasError,
-            [styles.placeholder]: !value,
+            [styles.inputError!]: hasError,
+            [styles.placeholder!]: !value,
           }),
           autoCorrect: "off",
           autoCapitalize: "off",

app/components/ExpandingToolbar.tsx
@@ -96,7 +96,7 @@ export default function ExpandingToolbar<T extends string>({
         {React.Children.map(children, (child) => {
           return (
             <Button
-              className={cx(styles.tab, { [styles.selected]: child === selectedChild })}
+              className={cx(styles.tab, { [styles.selected!]: child === selectedChild })}
               onClick={() => onSelectTab(child.props.name as any)}
             >
               {child.props.name}

app/components/Flex.tsx
@@ -75,15 +75,15 @@ const Flex = (props: Props) => {
 
   // toggle conditional classes based on props
   const conditionalClasses = {
-    [styles.col]: col,
-    [styles.reverse]: reverse,
-    [styles.center]: center,
-    [styles.start]: start,
-    [styles.end]: end,
-    [styles.wrap]: wrap,
-    [styles.clip]: clip,
-    [styles.scroll]: scroll,
-    [styles.scrollX]: scrollX,
+    [styles.col!]: col,
+    [styles.reverse!]: reverse,
+    [styles.center!]: center,
+    [styles.start!]: start,
+    [styles.end!]: end,
+    [styles.wrap!]: wrap,
+    [styles.clip!]: clip,
+    [styles.scroll!]: scroll,
+    [styles.scrollX!]: scrollX,
   };
   const combinedClasses = cx(styles.flex, conditionalClasses, className);
 

app/components/GlobalVariablesMenu.tsx
@@ -97,10 +97,14 @@ function GlobalVariablesMenu(props: Props) {
     setIsOpen(false);
     dispatch(addPanel({ type: GlobalVariables.panelType, layout } as AddPanelPayload));
 
-    logEvent({
-      name: getEventNames().PANEL_ADD,
-      tags: { [getEventTags().PANEL_TYPE]: GlobalVariables.panelType },
-    });
+    const name = getEventNames().PANEL_ADD;
+    const type = getEventTags().PANEL_TYPE;
+    if (name != undefined && type != undefined) {
+      logEvent({
+        name,
+        tags: { [type]: GlobalVariables.panelType },
+      });
+    }
   }, [dispatch, layout]);
 
   const { globalVariables } = useGlobalVariables();

app/components/Icon.tsx
@@ -58,15 +58,15 @@ const Icon = (props: Props) => {
     dataTest,
   } = props;
   const classNames = cx("icon", styles.icon, className, {
-    [styles.fade]: fade,
-    [styles.clickable]: !!onClick || clickable == undefined || clickable,
-    [styles.active]: active,
-    [styles.xlarge]: xlarge,
-    [styles.large]: large,
-    [styles.medium]: medium,
-    [styles.small]: small,
-    [styles.xsmall]: xsmall,
-    [styles.xxsmall]: xxsmall,
+    [styles.fade!]: fade,
+    [styles.clickable!]: !!onClick || clickable == undefined || clickable,
+    [styles.active!]: active,
+    [styles.xlarge!]: xlarge,
+    [styles.large!]: large,
+    [styles.medium!]: medium,
+    [styles.small!]: small,
+    [styles.xsmall!]: xsmall,
+    [styles.xxsmall!]: xxsmall,
   });
 
   // if we have a click handler

commit 1e608a4c8477b5e1483f06ed5b30148d758d7514

Fix Split Panel action when panel has no config (#579)
Broken in #321.

app/components/PanelToolbar/index.tsx
@@ -122,11 +122,7 @@ function StandardMenuItems({
         });
       }
 
-      const config = savedProps[id];
-      if (!config) {
-        return;
-      }
-
+      const config = savedProps[id] ?? {};
       actions.splitPanel({
         id,
         tabId,