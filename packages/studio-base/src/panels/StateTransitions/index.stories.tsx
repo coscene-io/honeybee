coscene-io/honeybee

commit 1d132746fb6d31480793723a111cd9b56f924787

Feature: add preloaded data to state transitions (#1827)
Display available preloaded data in state transitions.

Fixes: #689

packages/studio-base/src/panels/StateTransitions/index.stories.tsx
@@ -14,6 +14,7 @@
 import { useCallback } from "react";
 import TestUtils from "react-dom/test-utils";
 
+import { BlockCache } from "@foxglove/studio-base/randomAccessDataProviders/MemoryCacheDataProvider";
 import PanelSetup from "@foxglove/studio-base/stories/PanelSetup";
 import { useReadySignal } from "@foxglove/studio-base/stories/ReadySignalContext";
 
@@ -72,7 +73,10 @@ const fixture = {
       },
     }),
   ),
-  topics: [{ name: "/some/topic/with/state", datatype: "msgs/SystemState" }],
+  topics: [
+    { name: "/some/topic/with/state", datatype: "msgs/SystemState" },
+    { name: "/blocks", datatype: "msgs/SystemState" },
+  ],
   activeData: {
     startTime: { sec: 1526191527, nsec: 202050 },
     endTime: { sec: 1526191551, nsec: 999997069 },
@@ -189,35 +193,61 @@ export function JsonPath(): JSX.Element {
   );
 }
 
-WithAHoveredTooltip.parameters = {
-  chromatic: { delay: 200 },
-  useReadySignal: true,
+const messageCache: BlockCache = {
+  blocks: [
+    {
+      sizeInBytes: 0,
+      messagesByTopic: {
+        "/blocks": systemStateMessages.slice(0, 5).map((message, idx) => ({
+          topic: "/blocks",
+          receiveTime: message.header.stamp,
+          message: { ...message, data: { value: idx } },
+        })),
+      },
+    },
+    {
+      sizeInBytes: 0,
+      messagesByTopic: {
+        "/blocks": systemStateMessages.slice(7, 12).map((message, idx) => ({
+          topic: "/blocks",
+          receiveTime: message.header.stamp,
+          message: { ...message, data: { value: idx } },
+        })),
+      },
+    },
+    {
+      sizeInBytes: 0,
+      messagesByTopic: {},
+    },
+    {
+      sizeInBytes: 0,
+      messagesByTopic: {
+        "/blocks": systemStateMessages.slice(15, 19).map((message, idx) => ({
+          topic: "/blocks",
+          receiveTime: message.header.stamp,
+          message: { ...message, data: { value: idx } },
+        })),
+      },
+    },
+  ],
+  startTime: systemStateMessages[0]!.header.stamp,
 };
-export function WithAHoveredTooltip(): JSX.Element {
+
+Blocks.parameters = { useReadySignal: true };
+export function Blocks(): JSX.Element {
   const readySignal = useReadySignal({ count: 3 });
   const pauseFrame = useCallback(() => readySignal, [readySignal]);
+
+  const blockFixture = { ...fixture, progress: { messageCache } };
+
   return (
-    <PanelSetup
-      fixture={fixture}
-      pauseFrame={pauseFrame}
-      onMount={() => {
-        setTimeout(() => {
-          const [canvas] = document.getElementsByTagName("canvas");
-          const x = 163;
-          const y = 266;
-          canvas?.dispatchEvent(
-            new MouseEvent("mousemove", { screenX: x, clientX: x, screenY: y, clientY: y }),
-          );
-        }, 100);
-      }}
-      style={{ width: 370 }}
-    >
+    <PanelSetup fixture={blockFixture} pauseFrame={pauseFrame}>
       <StateTransitions
         overrideConfig={{
-          paths: new Array(5).fill({
-            value: "/some/topic/with/state.state",
-            timestampMethod: "receiveTime",
-          }),
+          paths: [
+            { value: "/some/topic/with/state.state", timestampMethod: "receiveTime" },
+            { value: "/blocks.state", timestampMethod: "receiveTime" },
+          ],
         }}
       />
     </PanelSetup>

packages/studio-base/src/panels/StateTransitions/index.tsx
@@ -13,33 +13,37 @@
 
 import { ChartOptions, ScaleOptions } from "chart.js";
 import { uniq } from "lodash";
-import { ComponentProps, useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo } from "react";
 import { useResizeDetector } from "react-resize-detector";
-import stringHash from "string-hash";
 import styled, { css } from "styled-components";
 import tinycolor from "tinycolor2";
 
-import { add as addTimes, subtract as subtractTimes, toSec, fromSec } from "@foxglove/rostime";
+import { useShallowMemo } from "@foxglove/hooks";
+import { add as addTimes, fromSec } from "@foxglove/rostime";
 import * as PanelAPI from "@foxglove/studio-base/PanelAPI";
+import { useBlocksByTopic } from "@foxglove/studio-base/PanelAPI";
 import Button from "@foxglove/studio-base/components/Button";
 import MessagePathInput from "@foxglove/studio-base/components/MessagePathSyntax/MessagePathInput";
+import { getTopicsFromPaths } from "@foxglove/studio-base/components/MessagePathSyntax/parseRosPath";
+import { useDecodeMessagePathsForMessagesByTopic } from "@foxglove/studio-base/components/MessagePathSyntax/useCachedGetMessagePathDataItems";
 import useMessagesByPath from "@foxglove/studio-base/components/MessagePathSyntax/useMessagesByPath";
 import { useMessagePipelineGetter } from "@foxglove/studio-base/components/MessagePipeline";
 import Panel from "@foxglove/studio-base/components/Panel";
 import PanelToolbar from "@foxglove/studio-base/components/PanelToolbar";
 import TimeBasedChart, {
-  getTooltipItemForMessageHistoryItem,
   TimeBasedChartTooltipData,
 } from "@foxglove/studio-base/components/TimeBasedChart";
-import { OnClickArg as OnChartClickArgs } from "@foxglove/studio-base/src/components/Chart";
+import {
+  ChartData,
+  OnClickArg as OnChartClickArgs,
+} from "@foxglove/studio-base/src/components/Chart";
 import { PanelConfig } from "@foxglove/studio-base/types/panels";
-import { darkColor, lineColors } from "@foxglove/studio-base/util/plotColors";
 import { colors, fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 import { TimestampMethod } from "@foxglove/studio-base/util/time";
-import { grey } from "@foxglove/studio-base/util/toolsColorScheme";
 
 import helpContent from "./index.help.md";
-import positiveModulo from "./positiveModulo";
+import messagesToDatasets from "./messagesToDatasets";
+import { StateTransitionPath } from "./types";
 
 export const transitionableRosTypes = [
   "bool",
@@ -164,7 +168,6 @@ const plugins: ChartOptions["plugins"] = {
   },
 };
 
-export type StateTransitionPath = { value: string; timestampMethod: TimestampMethod };
 export type StateTransitionConfig = { paths: StateTransitionPath[] };
 
 export function openSiblingStateTransitionsPanel(
@@ -216,9 +219,19 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
     saveConfig({ paths: newPaths });
   };
 
-  const [defaultStart] = useState({ sec: 0, nsec: 0 });
-  const { startTime = defaultStart } = PanelAPI.useDataSourceInfo();
-  const itemsByPath = useMessagesByPath(useMemo(() => paths.map(({ value }) => value), [paths]));
+  const pathStrings = useMemo(() => paths.map(({ value }) => value), [paths]);
+  const subscribeTopics = useMemo(() => getTopicsFromPaths(pathStrings), [pathStrings]);
+
+  const { startTime } = PanelAPI.useDataSourceInfo();
+  const itemsByPath = useMessagesByPath(pathStrings);
+
+  const decodeMessagePathsForMessagesByTopic = useDecodeMessagePathsForMessagesByTopic(pathStrings);
+
+  const blocks = useBlocksByTopic(subscribeTopics);
+  const decodedBlocks = useMemo(
+    () => blocks.map(decodeMessagePathsForMessagesByTopic),
+    [blocks, decodeMessagePathsForMessagesByTopic],
+  );
 
   const { height, heightPerTopic } = useMemo(() => {
     const onlyTopicsHeight = paths.length * 55;
@@ -229,125 +242,61 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
     };
   }, [paths.length]);
 
-  const baseColors = useMemo(() => {
-    return [grey, ...lineColors];
-  }, []);
-
   const { datasets, tooltips, minY } = useMemo(() => {
     let outMinY: number | undefined;
 
     const outTooltips: TimeBasedChartTooltipData[] = [];
-    const outDatasets: typeof data["datasets"] = [];
+    const outDatasets: ChartData["datasets"] = [];
+
+    // ignore all data when we don't have a start time
+    if (!startTime) {
+      return {
+        datasets: outDatasets,
+        tooltips: outTooltips,
+        minY: outMinY,
+      };
+    }
 
     let pathIndex = 0;
     for (const path of paths) {
-      const { value: pathValue, timestampMethod } = path;
-
-      let prevQueryValue;
-      let previousTimestamp;
-      let currentData: typeof outDatasets[0]["data"] = [];
-
       // y axis values are set based on the path we are rendering
       // negative makes each path render below the previous
       const y = (pathIndex + 1) * 6 * -1;
       outMinY = Math.min(outMinY ?? y, y - 3);
 
-      for (const itemByPath of itemsByPath[pathValue] ?? []) {
-        const item = getTooltipItemForMessageHistoryItem(itemByPath);
-        const timestamp = timestampMethod === "headerStamp" ? item.headerStamp : item.receiveTime;
-        if (!timestamp) {
-          continue;
-        }
+      const blocksForPath = decodedBlocks.map((decodedBlock) => decodedBlock[path.value]);
 
-        const queriedData = item.queriedData[0];
-        if (item.queriedData.length !== 1 || !queriedData) {
-          continue;
-        }
-
-        const { constantName, value } = queriedData;
-
-        // Skip duplicates.
-        if (
-          previousTimestamp &&
-          toSec(subtractTimes(previousTimestamp, timestamp)) === 0 &&
-          prevQueryValue === value
-        ) {
-          continue;
-        }
-        previousTimestamp = timestamp;
-
-        // Skip anything that cannot be cast to a number or is a string.
-        if (Number.isNaN(value) && typeof value !== "string") {
-          continue;
-        }
-
-        if (typeof value !== "number" && typeof value !== "boolean" && typeof value !== "string") {
-          continue;
-        }
-
-        const valueForColor =
-          typeof value === "string" ? stringHash(value) : Math.round(Number(value));
-        const color =
-          baseColors[positiveModulo(valueForColor, Object.values(baseColors).length)] ?? "grey";
-
-        const x = toSec(subtractTimes(timestamp, startTime));
-
-        const element = {
-          x,
-          y,
-        };
-
-        const tooltip: TimeBasedChartTooltipData = {
-          x,
-          y,
-          item,
-          path: pathValue,
-          value,
-          constantName,
+      {
+        const { datasets: newDataSets, tooltips: newTooltips } = messagesToDatasets({
+          path,
           startTime,
-        };
-        outTooltips.unshift(tooltip);
-
-        // the current point is added even if different from previous value to avoid _gaps_ in the data
-        // this is a myproduct of using separate datasets to render each color
-        currentData.push({
-          x,
           y,
+          pathIndex,
+          blocks: blocksForPath,
         });
 
-        // if the value is different from previous value, make a new dataset
-        if (value !== prevQueryValue) {
-          const label =
-            constantName != undefined ? `${constantName} (${String(value)})` : String(value);
+        outDatasets.push(...newDataSets);
+        outTooltips.push(...newTooltips);
+      }
 
-          const elementWithLabel = {
-            ...element,
-            label,
-            labelColor: color,
-          };
+      // If we have have messages in blocks for this path, we ignore streamed messages and only
+      // display the messages from blocks.
+      const haveBlocksForPath = blocksForPath.some((item) => item != undefined);
+      if (haveBlocksForPath) {
+        continue;
+      }
 
-          // new data starts with our current point, the current point
-          currentData = [elementWithLabel];
-          const dataset: typeof data["datasets"][0] = {
-            borderWidth: 10,
-            borderColor: color,
-            data: currentData,
-            label: pathIndex.toString(),
-            pointBackgroundColor: darkColor(color),
-            pointBorderColor: "transparent",
-            pointHoverRadius: 3,
-            pointRadius: 1.25,
-            pointStyle: "circle",
-            showLine: true,
-            datalabels: {
-              color,
-            },
-          };
-
-          outDatasets.push(dataset);
-        }
-
-        prevQueryValue = value;
+      const items = itemsByPath[path.value];
+      if (items) {
+        const { datasets: newDataSets, tooltips: newTooltips } = messagesToDatasets({
+          path,
+          startTime,
+          y,
+          pathIndex,
+          blocks: [items],
+        });
+        outDatasets.push(...newDataSets);
+        outTooltips.push(...newTooltips);
       }
 
       ++pathIndex;
@@ -358,11 +307,7 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
       tooltips: outTooltips,
       minY: outMinY,
     };
-  }, [baseColors, itemsByPath, paths, startTime]);
-
-  const data: ComponentProps<typeof TimeBasedChart>["data"] = {
-    datasets,
-  };
+  }, [itemsByPath, decodedBlocks, paths, startTime]);
 
   const yScale = useMemo<ScaleOptions>(() => {
     return {
@@ -404,6 +349,8 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
     },
     [messagePipeline],
   );
+
+  const data: ChartData = useShallowMemo({ datasets });
 
   return (
     <SRoot>

packages/studio-base/src/components/Chart/index.tsx
@@ -8,7 +8,7 @@
 /// <reference types="chartjs-plugin-datalabels" />
 /// <reference types="chartjs-plugin-zoom" />
 
-import { ChartOptions, ChartData, ScatterDataPoint } from "chart.js";
+import { ChartOptions, ChartData as ChartJsChartData, ScatterDataPoint } from "chart.js";
 import Hammer from "hammerjs";
 import { useCallback, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { useAsync, useMountedState } from "react-use";
@@ -32,10 +32,10 @@ export type OnClickArg = {
 // Chartjs typings use _null_ to indicate _gaps_ in the dataset
 // eslint-disable-next-line no-restricted-syntax
 const ChartNull = null;
-type Data = ChartData<"scatter", (ScatterDataPoint | typeof ChartNull)[]>;
+export type ChartData = ChartJsChartData<"scatter", (ScatterDataPoint | typeof ChartNull)[]>;
 
 type Props = {
-  data: Data;
+  data: ChartData;
   options: ChartOptions;
   type: string;
   height: number;

packages/studio-base/src/panels/StateTransitions/messagesToDatasets.ts
@@ -1,0 +1,151 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import stringHash from "string-hash";
+
+import { Time, toSec } from "@foxglove/rostime";
+import { ChartData } from "@foxglove/studio-base/components/Chart";
+import { MessageAndData } from "@foxglove/studio-base/components/MessagePathSyntax/useCachedGetMessagePathDataItems";
+import {
+  getTooltipItemForMessageHistoryItem,
+  TimeBasedChartTooltipData,
+} from "@foxglove/studio-base/components/TimeBasedChart";
+import { subtractTimes } from "@foxglove/studio-base/players/UserNodePlayer/nodeTransformerWorker/typescript/userUtils/time";
+import { darkColor, lineColors } from "@foxglove/studio-base/util/plotColors";
+import { grey } from "@foxglove/studio-base/util/toolsColorScheme";
+
+import positiveModulo from "./positiveModulo";
+import { StateTransitionPath } from "./types";
+
+type DatasetInfo = {
+  datasets: ChartData["datasets"];
+  tooltips: TimeBasedChartTooltipData[];
+};
+
+const baseColors = [grey, ...lineColors];
+
+type Args = {
+  path: StateTransitionPath;
+  startTime: Time;
+  y: number;
+  pathIndex: number;
+  blocks: readonly (readonly MessageAndData[] | undefined)[];
+};
+
+export default function messagesToDatasets(args: Args): DatasetInfo {
+  const { path, startTime, y, pathIndex, blocks } = args;
+
+  const info: DatasetInfo = {
+    datasets: [],
+    tooltips: [],
+  };
+
+  let prevQueryValue: string | number | boolean | undefined;
+  let previousTimestamp: Time | undefined;
+  let currentData: ChartData["datasets"][0]["data"] = [];
+
+  for (const messages of blocks) {
+    if (!messages) {
+      currentData = [];
+      prevQueryValue = undefined;
+      previousTimestamp = undefined;
+      continue;
+    }
+
+    for (const itemByPath of messages) {
+      const item = getTooltipItemForMessageHistoryItem(itemByPath);
+      const timestamp =
+        path.timestampMethod === "headerStamp" ? item.headerStamp : item.receiveTime;
+      if (!timestamp) {
+        continue;
+      }
+
+      const queriedData = item.queriedData[0];
+      if (item.queriedData.length !== 1 || !queriedData) {
+        continue;
+      }
+
+      const { constantName, value } = queriedData;
+
+      // Skip duplicates.
+      if (
+        previousTimestamp &&
+        toSec(subtractTimes(previousTimestamp, timestamp)) === 0 &&
+        prevQueryValue === value
+      ) {
+        continue;
+      }
+      previousTimestamp = timestamp;
+
+      // Skip anything that cannot be cast to a number or is a string.
+      if (Number.isNaN(value) && typeof value !== "string") {
+        continue;
+      }
+
+      if (typeof value !== "number" && typeof value !== "boolean" && typeof value !== "string") {
+        continue;
+      }
+
+      const valueForColor =
+        typeof value === "string" ? stringHash(value) : Math.round(Number(value));
+      const color =
+        baseColors[positiveModulo(valueForColor, Object.values(baseColors).length)] ?? "grey";
+
+      const x = toSec(subtractTimes(timestamp, startTime));
+
+      const element = { x, y };
+
+      const tooltip: TimeBasedChartTooltipData = {
+        x,
+        y,
+        item,
+        path: path.value,
+        value,
+        constantName,
+        startTime,
+      };
+      info.tooltips.unshift(tooltip);
+
+      // the current point is added even if different from previous value to avoid _gaps_ in the data
+      // this is a byproduct of using separate datasets to render each color
+      currentData.push({ x, y });
+
+      // if the value is different from previous value, make a new dataset
+      if (value !== prevQueryValue) {
+        const label =
+          constantName != undefined ? `${constantName} (${String(value)})` : String(value);
+
+        const elementWithLabel = {
+          ...element,
+          label,
+          labelColor: color,
+        };
+
+        // new data starts with our current point, the current point
+        currentData = [elementWithLabel];
+        const dataset: ChartData["datasets"][0] = {
+          borderWidth: 10,
+          borderColor: color,
+          data: currentData,
+          label: pathIndex.toString(),
+          pointBackgroundColor: darkColor(color),
+          pointBorderColor: "transparent",
+          pointHoverRadius: 3,
+          pointRadius: 1.25,
+          pointStyle: "circle",
+          showLine: true,
+          datalabels: {
+            color,
+          },
+        };
+
+        info.datasets.push(dataset);
+      }
+
+      prevQueryValue = value;
+    }
+  }
+
+  return info;
+}

packages/studio-base/src/panels/StateTransitions/types.ts
@@ -1,0 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { TimestampMethod } from "@foxglove/studio-base/util/time";
+
+export type StateTransitionPath = { value: string; timestampMethod: TimestampMethod };

commit 2b5a3807951feb6ec0fe765eb69140ce980cd838

Fix State Transitions y alignment for preloaded data (#1863)
**User-Facing Changes**
Fixes an issue with State Transitions where multiple preloaded datasets would overlap.

**Description**
Fixes https://github.com/foxglove/studio/issues/1861, introduced in #1827.

packages/studio-base/src/panels/StateTransitions/index.stories.tsx
@@ -247,6 +247,7 @@ export function Blocks(): JSX.Element {
           paths: [
             { value: "/some/topic/with/state.state", timestampMethod: "receiveTime" },
             { value: "/blocks.state", timestampMethod: "receiveTime" },
+            { value: "/blocks.state", timestampMethod: "receiveTime" },
           ],
         }}
       />

packages/studio-base/src/panels/StateTransitions/index.tsx
@@ -270,8 +270,7 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
       };
     }
 
-    let pathIndex = 0;
-    for (const path of paths) {
+    paths.forEach((path, pathIndex) => {
       // y axis values are set based on the path we are rendering
       // negative makes each path render below the previous
       const y = (pathIndex + 1) * 6 * -1;
@@ -296,7 +295,7 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
       // display the messages from blocks.
       const haveBlocksForPath = blocksForPath.some((item) => item != undefined);
       if (haveBlocksForPath) {
-        continue;
+        return;
       }
 
       const items = itemsByPath[path.value];
@@ -311,9 +310,7 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
         outDatasets.push(...newDataSets);
         outTooltips.push(...newTooltips);
       }
-
-      ++pathIndex;
-    }
+    });
 
     return {
       datasets: outDatasets,