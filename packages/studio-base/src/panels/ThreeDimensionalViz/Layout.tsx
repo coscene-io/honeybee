Lichtblick-Suite/lichtblick

commit 18ee39131d65b6172c001e1a96b1acf985c759ac

Remove internal studio fields from 3d panel selected object details popup  (#2617)
When a user selects an object in the 3d panel, the object details panel opens with details on the object. This panel would confusingly show internal implementation fields alongside the marker fields.

This change updates the display to only show fields from the original marker message.

Fixes: #2476

packages/studio-base/src/panels/ThreeDimensionalViz/Layout.tsx
@@ -602,22 +602,15 @@ export default function Layout({
     toggleDebug,
   } = useMemo(() => {
     return {
-      onClick: (ev: React.MouseEvent, args?: ReglClickInfo) => {
+      onClick: (_ev: React.MouseEvent, args?: ReglClickInfo) => {
         // Don't set any clicked objects when measuring distance or drawing polygons.
         if (callbackInputsRef.current.isDrawing) {
           return;
         }
         const newClickedObjects =
           (args?.objects as MouseEventObject[] | undefined) ?? ([] as MouseEventObject[]);
-        const newClickedPosition = { clientX: ev.clientX, clientY: ev.clientY };
         const newSelectedObject = newClickedObjects.length === 1 ? newClickedObjects[0] : undefined;
 
-        // Select the object directly if there is only one or open up context menu if there are many.
-        setSelectionState({
-          ...callbackInputsRef.current.selectionState,
-          clickedObjects: newClickedObjects,
-          clickedPosition: newClickedPosition,
-        });
         selectObject(newSelectedObject);
       },
       onControlsOverlayClick: (ev: React.MouseEvent<HTMLDivElement>) => {

packages/studio-base/src/panels/ThreeDimensionalViz/Interactions/Interaction.stories.tsx
@@ -281,7 +281,7 @@ storiesOf("panels/ThreeDimensionalViz/Interactions/Interaction", module)
               object: {
                 ...cloud1,
                 type: 102,
-                interactionData: { topic: "/foo/bar", originalMessage: selectedObject.object },
+                interactionData: { topic: "/foo/bar", originalMessage: POINT_CLOUD_MESSAGE },
               },
             }}
           />
@@ -294,7 +294,10 @@ storiesOf("panels/ThreeDimensionalViz/Interactions/Interaction", module)
               object: {
                 ...cloud2,
                 type: 102,
-                interactionData: { topic: "/foo/bar", originalMessage: selectedObject.object },
+                interactionData: {
+                  topic: "/foo/bar",
+                  originalMessage: POINT_CLOUD_WITH_ADDITIONAL_FIELDS,
+                },
               },
             }}
           />

packages/studio-base/src/panels/ThreeDimensionalViz/Interactions/Interactions.tsx
@@ -55,17 +55,20 @@ const InteractionsBaseComponent = React.memo<PropsWithConfig>(function Interacti
   saveConfig,
 }: PropsWithConfig) {
   const { object } = selectedObject ?? {};
-  const isPointCloud = object ? (object as unknown as { type: number }).type === 102 : false;
+  const selectedInteractionData = getInteractionData(selectedObject);
+
+  const { originalMessage } = selectedInteractionData ?? {};
+
+  const isPointCloud = (object as { type?: number })?.type === 102;
   const maybeFullyDecodedObject = React.useMemo(
     () =>
       isPointCloud
-        ? { ...selectedObject, object: decodeAdditionalFields(object as unknown as PointCloud2) }
-        : selectedObject,
-    [isPointCloud, object, selectedObject],
+        ? decodeAdditionalFields(originalMessage as unknown as PointCloud2)
+        : originalMessage,
+    [isPointCloud, originalMessage],
   );
 
   const { linkedGlobalVariables } = useLinkedGlobalVariables();
-  const selectedInteractionData = selectedObject && getInteractionData(selectedObject);
 
   return (
     <ExpandingToolbar
@@ -86,10 +89,10 @@ const InteractionsBaseComponent = React.memo<PropsWithConfig>(function Interacti
                 </SRow>
               )}
               {isPointCloud && (
-                <PointCloudDetails selectedObject={maybeFullyDecodedObject as SelectedObject} />
+                <PointCloudDetails selectedObject={selectedObject as SelectedObject} />
               )}
               <ObjectDetails
-                selectedObject={maybeFullyDecodedObject as SelectedObject}
+                selectedObject={maybeFullyDecodedObject}
                 interactionData={selectedInteractionData}
               />
             </>

packages/studio-base/src/panels/ThreeDimensionalViz/Interactions/ObjectDetails.tsx
@@ -12,16 +12,11 @@
 //   You may not use this file except in compliance with the License.
 
 import { first, omit, sortBy } from "lodash";
-import { useCallback } from "react";
 import Tree from "react-json-tree";
 import styled from "styled-components";
 
-import { MouseEventObject } from "@foxglove/regl-worldview";
-import Dropdown from "@foxglove/studio-base/components/Dropdown";
-import DropdownItem from "@foxglove/studio-base/components/Dropdown/DropdownItem";
 import useGetItemStringWithTimezone from "@foxglove/studio-base/components/JsonTree/useGetItemStringWithTimezone";
-import styles from "@foxglove/studio-base/panels/ThreeDimensionalViz/sharedStyles";
-import { getInstanceObj } from "@foxglove/studio-base/panels/ThreeDimensionalViz/threeDimensionalVizUtils";
+import { RosValue } from "@foxglove/studio-base/players/types";
 import { useJsonTreeTheme } from "@foxglove/studio-base/util/globalConstants";
 
 import GlobalVariableLink from "./GlobalVariableLink/index";
@@ -41,57 +36,14 @@ const PREFERRED_OBJECT_KEY_ORDER = [
   "points",
 ].reverse();
 
-const SObjectDetails = styled.div`
+const TreeContainer = styled.div`
   padding: 12px 0 16px 0;
 `;
 
-type CommonProps = { readonly interactionData?: InteractionData };
-
-type WrapperProps = CommonProps & { readonly selectedObject: MouseEventObject };
-
-type Props = CommonProps & { readonly objectToDisplay: unknown };
-
-// Used for switching between views of individual and combined objects.
-// TODO(steel): Only show the combined object when the individual objects are semantically related.
-export default function ObjectDetails({
-  interactionData,
-  selectedObject: { object, instanceIndex },
-}: WrapperProps): JSX.Element {
-  const [showInstance, setShowInstance] = React.useState(true);
-  const instanceObject = getInstanceObj(object, instanceIndex as number);
-  const dropdownText = {
-    instance: "Show instance object",
-    full: "Show full object",
-  };
-
-  // eslint-disable-next-line @foxglove/no-boolean-parameters
-  const updateShowInstance = useCallback((shouldShowInstance: boolean) => {
-    setShowInstance(shouldShowInstance);
-  }, []);
-
-  const objectToDisplay = instanceObject != undefined && showInstance ? instanceObject : object;
-  return (
-    <div>
-      {instanceObject != undefined && (
-        <Dropdown
-          btnClassname={styles.button}
-          position="below"
-          value={showInstance}
-          text={showInstance ? dropdownText.instance : dropdownText.full}
-          onChange={updateShowInstance}
-        >
-          <DropdownItem value={true}>
-            <span>{dropdownText.instance}</span>
-          </DropdownItem>
-          <DropdownItem value={false}>
-            <span>{dropdownText.full}</span>
-          </DropdownItem>
-        </Dropdown>
-      )}
-      <ObjectDetailsInner interactionData={interactionData} objectToDisplay={objectToDisplay} />
-    </div>
-  );
-}
+type Props = {
+  readonly interactionData?: InteractionData;
+  readonly selectedObject?: RosValue;
+};
 
 function maybePlainObject(rawVal: unknown) {
   if (typeof rawVal === "object" && rawVal && "toJSON" in rawVal) {
@@ -100,16 +52,13 @@ function maybePlainObject(rawVal: unknown) {
   return rawVal;
 }
 
-function ObjectDetailsInner({ interactionData, objectToDisplay }: Props) {
+function ObjectDetails({ interactionData, selectedObject }: Props): JSX.Element {
   const jsonTreeTheme = useJsonTreeTheme();
   const topic = interactionData?.topic ?? "";
 
   // object to display may not be a plain-ole-data
   // We need a plain object to sort the keys and omit interaction data
-  const plainObject =
-    "toJSON" in (objectToDisplay as { toJSON?: () => unknown })
-      ? (objectToDisplay as { toJSON: () => unknown }).toJSON()
-      : objectToDisplay;
+  const plainObject = maybePlainObject(selectedObject);
   const originalObject = omit(plainObject as Record<string, unknown>, "interactionData");
 
   const getItemString = useGetItemStringWithTimezone();
@@ -117,16 +66,16 @@ function ObjectDetailsInner({ interactionData, objectToDisplay }: Props) {
   if (topic.length === 0) {
     // show the original object directly if there is no interaction data
     return (
-      <SObjectDetails>
+      <TreeContainer>
         <Tree
-          data={objectToDisplay}
+          data={selectedObject}
           shouldExpandNode={(_markerKeyPath, _data, level) => level < 2}
           invertTheme={false}
           postprocessValue={maybePlainObject}
           theme={{ ...jsonTreeTheme, tree: { margin: 0 } }}
           hideRoot
         />
-      </SObjectDetails>
+      </TreeContainer>
     );
   }
 
@@ -137,7 +86,7 @@ function ObjectDetailsInner({ interactionData, objectToDisplay }: Props) {
   );
 
   return (
-    <SObjectDetails>
+    <TreeContainer>
       <Tree
         data={sortedDataObject}
         shouldExpandNode={() => false}
@@ -190,6 +139,8 @@ function ObjectDetailsInner({ interactionData, objectToDisplay }: Props) {
           );
         }}
       />
-    </SObjectDetails>
+    </TreeContainer>
   );
 }
+
+export default ObjectDetails;

packages/studio-base/src/panels/ThreeDimensionalViz/SceneBuilder/index.ts
@@ -654,11 +654,8 @@ export default class SceneBuilder implements MarkerProvider {
     const decayTimeInSec = this._settingsByKey[`t:${topic}`]?.decayTime as number | undefined;
     const lifetime =
       decayTimeInSec != undefined && decayTimeInSec !== 0 ? fromSec(decayTimeInSec) : undefined;
-    (this.collectors[topic] as MessageCollector).addNonMarker(
-      topic,
-      mappedMessage as Interactive<unknown>,
-      lifetime,
-    );
+
+    this.collectors[topic]?.addNonMarker(topic, mappedMessage as Interactive<unknown>, lifetime);
   };
 
   setCurrentTime = (currentTime: { sec: number; nsec: number }): void => {
@@ -671,7 +668,7 @@ export default class SceneBuilder implements MarkerProvider {
     }
   };
 
-  // extracts renderable markers from the ros frame
+  // extracts renderable markers from the frame
   render(): void {
     for (const topic of this.topicsToRender) {
       try {
@@ -879,13 +876,10 @@ export default class SceneBuilder implements MarkerProvider {
         );
         marker.interactionData.highlighted = markerMatches;
 
-        // TODO(bmc): once we support more topic settings
-        // flesh this out to be more marker type agnostic
         const settings = this._settingsByKey[`t:${topic.name}`];
         if (settings) {
           (marker as { settings?: unknown }).settings = settings;
         }
-
         this._addMarkerToCollector(add, topic, marker, pose);
       }
 

packages/studio-base/src/panels/ThreeDimensionalViz/commands/PointClouds/selection.ts
@@ -11,7 +11,7 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-import { omit, difference, isEmpty, isNil } from "lodash";
+import { difference, isEmpty, isNil } from "lodash";
 
 import { toRGBA, Color } from "@foxglove/regl-worldview";
 import {
@@ -20,6 +20,7 @@ import {
   DEFAULT_MAX_COLOR,
 } from "@foxglove/studio-base/panels/ThreeDimensionalViz/TopicSettingsEditor/PointCloudSettingsEditor";
 import { DecodedMarker } from "@foxglove/studio-base/panels/ThreeDimensionalViz/commands/PointClouds/decodeMarker";
+import { RosObject } from "@foxglove/studio-base/players/types";
 import { PointCloud2, PointField } from "@foxglove/studio-base/types/Messages";
 
 import {
@@ -154,8 +155,10 @@ export function getAdditionalFieldNames(fields: readonly PointField[]): string[]
 
 export function decodeAdditionalFields<T extends PointCloud2>(
   marker: T,
-): Omit<T, "data"> & Record<string, unknown> {
-  const { fields, data, width, row_step, height, point_step } = marker;
+): Omit<T, "data"> & RosObject {
+  const { data, ...markerRest } = marker;
+  const { fields, width, row_step, height, point_step } = markerRest;
+
   const offsets = getFieldOffsetsAndReaders(data, fields);
 
   let pointCount = 0;
@@ -181,7 +184,7 @@ export function decodeAdditionalFields<T extends PointCloud2>(
   }
 
   return {
-    ...omit(marker, "data"), // no need to include data since all fields have been decoded
+    ...markerRest, // no need to include data since all fields have been decoded
     ...otherFieldsValues,
   };
 }

packages/studio-base/src/types/Messages.ts
@@ -141,7 +141,6 @@ type ArrowSize = Readonly<{
   headWidth: number;
 }>;
 
-// TODO: Is this correct?
 export type ArrowMarker = Readonly<
   BaseMarker & {
     type: 0;

coscene-io/honeybee

commit 48e3f456953758eceb5d9035d095563e1f691d87

Show context menu when clicking overlapping markers (#2845)
**User-Facing Changes**
Clicking overlapping markers in the 3d panel shows a menu to select the specific marker.

**Description**
When clicking on overlapping markers in the 3d panel, display a menu to select the specific marker to inspect. Prior to this fix, clicking overlapping markers resulted in one being selected with no way to select others.

This functionality was broken by https://github.com/foxglove/studio/pull/2617

Fixes: #2844

packages/studio-base/src/panels/ThreeDimensionalViz/Layout.tsx
@@ -621,16 +621,31 @@ export default function Layout({
     toggleDebug,
   } = useMemo(() => {
     return {
-      onClick: (_ev: React.MouseEvent, args?: ReglClickInfo) => {
+      onClick: (ev: React.MouseEvent, args?: ReglClickInfo) => {
         // Don't set any clicked objects when measuring distance or drawing polygons.
         if (callbackInputsRef.current.isDrawing) {
           return;
         }
         const newClickedObjects =
           (args?.objects as MouseEventObject[] | undefined) ?? ([] as MouseEventObject[]);
-        const newSelectedObject = newClickedObjects.length === 1 ? newClickedObjects[0] : undefined;
 
-        selectObject(newSelectedObject);
+        // With multiple objects we update the selection state with all possible objects
+        if (newClickedObjects.length > 1) {
+          setSelectionState((prevState) => {
+            return {
+              ...prevState,
+              selectedObject: undefined,
+              clickedObjects: newClickedObjects,
+              clickedPosition: {
+                clientX: ev.clientX,
+                clientY: ev.clientY,
+              },
+            };
+          });
+          return;
+        }
+
+        selectObject(newClickedObjects[0]);
       },
       onControlsOverlayClick: (ev: React.MouseEvent<HTMLDivElement>) => {
         if (!containerRef.current) {