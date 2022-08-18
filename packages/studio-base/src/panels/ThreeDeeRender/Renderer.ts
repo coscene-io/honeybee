Lichtblick-Suite/lichtblick

commit 930ace21ea13205b083874d68ed445fc9c094531

[New3D] Revert camera settings back to degrees, remove @foxglove/regl-worldview dep from New3D, cleanup (#4043)

packages/studio-base/src/panels/ThreeDeeRender/Renderer.ts
@@ -10,7 +10,6 @@ import { DeepPartial } from "ts-essentials";
 import { v4 as uuidv4 } from "uuid";
 
 import Logger from "@foxglove/log";
-import { CameraState } from "@foxglove/regl-worldview";
 import { toNanoSec } from "@foxglove/rostime";
 import type { FrameTransform } from "@foxglove/schemas/schemas/typescript";
 import {
@@ -32,6 +31,7 @@ import type { Renderable } from "./Renderable";
 import { SceneExtension } from "./SceneExtension";
 import { ScreenOverlay } from "./ScreenOverlay";
 import { SettingsManager, SettingsTreeEntry } from "./SettingsManager";
+import { CameraState } from "./camera";
 import { stringToRgb } from "./color";
 import { FRAME_TRANSFORM_DATATYPES } from "./foxglove";
 import { DetailLevel, msaaSamples } from "./lod";
@@ -167,6 +167,7 @@ const LAYER_DEFAULT = 0;
 const LAYER_SELECTED = 1;
 
 const UNIT_X = new THREE.Vector3(1, 0, 0);
+const UNIT_Z = new THREE.Vector3(0, 0, 1);
 const PI_2 = Math.PI / 2;
 
 // Coordinate frames named in [REP-105](https://www.ros.org/reps/rep-0105.html)
@@ -181,7 +182,7 @@ const FRAME_NOT_FOUND = "FRAME_NOT_FOUND";
 const RENDERER_ID = "foxglove.Renderer";
 
 const tempColor = new THREE.Color();
-const tempVec = new THREE.Vector3();
+const tempVec3 = new THREE.Vector3();
 const tempVec2 = new THREE.Vector2();
 const tempSpherical = new THREE.Spherical();
 const tempEuler = new THREE.Euler();
@@ -611,52 +612,42 @@ export class Renderer extends EventEmitter<RendererEvents> {
     return topicCount === 0 && vizCount === 0 ? "Topics" : `Topics (${vizCount}/${topicCount})`;
   }
 
-  /** Translate a @foxglove/regl-worldview CameraState to the three.js coordinate system */
+  /** Translate a CameraState to the three.js coordinate system */
   private _updateCameras(cameraState: CameraState): void {
+    const targetOffset = tempVec3;
+    targetOffset.fromArray(cameraState.targetOffset);
+
+    const phi = THREE.MathUtils.degToRad(cameraState.phi);
+    const theta = -THREE.MathUtils.degToRad(cameraState.thetaOffset);
+
     if (cameraState.perspective) {
       // Unlock the polar angle (pitch axis)
       this.controls.minPolarAngle = 0;
       this.controls.maxPolarAngle = Math.PI;
 
-      this.perspectiveCamera.position
-        .setFromSpherical(
-          tempSpherical.set(cameraState.distance, cameraState.phi, -cameraState.thetaOffset),
-        )
-        .applyAxisAngle(UNIT_X, PI_2);
-      this.perspectiveCamera.position.add(
-        tempVec.set(
-          cameraState.targetOffset[0],
-          cameraState.targetOffset[1],
-          cameraState.targetOffset[2], // always 0 in Worldview CameraListener
-        ),
-      );
-      this.perspectiveCamera.quaternion.setFromEuler(
-        tempEuler.set(cameraState.phi, 0, -cameraState.thetaOffset, "ZYX"),
-      );
-      this.perspectiveCamera.fov = cameraState.fovy * (180 / Math.PI);
+      // Convert the camera spherical coordinates (radius, phi, theta) to Cartesian (X, Y, Z)
+      tempSpherical.set(cameraState.distance, phi, theta);
+      this.perspectiveCamera.position.setFromSpherical(tempSpherical).applyAxisAngle(UNIT_X, PI_2);
+
+      // Add the camera offset
+      this.perspectiveCamera.position.add(targetOffset);
+
+      // Convert the camera spherical coordinates (phi, theta) to a quaternion rotation
+      this.perspectiveCamera.quaternion.setFromEuler(tempEuler.set(phi, 0, theta, "ZYX"));
+      this.perspectiveCamera.fov = cameraState.fovy;
       this.perspectiveCamera.near = cameraState.near;
       this.perspectiveCamera.far = cameraState.far;
       this.perspectiveCamera.aspect = this.aspect;
       this.perspectiveCamera.updateProjectionMatrix();
-      this.controls.target.set(
-        cameraState.targetOffset[0],
-        cameraState.targetOffset[1],
-        cameraState.targetOffset[2],
-      );
+
+      this.controls.target.copy(targetOffset);
     } else {
       // Lock the polar angle during 2D mode
       const curPolarAngle = this.controls.getPolarAngle();
       this.controls.minPolarAngle = this.controls.maxPolarAngle = curPolarAngle;
 
-      this.orthographicCamera.position.set(
-        cameraState.targetOffset[0],
-        cameraState.targetOffset[1],
-        cameraState.far / 2,
-      );
-      this.orthographicCamera.quaternion.setFromAxisAngle(
-        tempVec.set(0, 0, 1),
-        -cameraState.thetaOffset,
-      );
+      this.orthographicCamera.position.set(targetOffset.x, targetOffset.y, cameraState.far / 2);
+      this.orthographicCamera.quaternion.setFromAxisAngle(UNIT_Z, theta);
       this.orthographicCamera.left = (-cameraState.distance / 2) * this.aspect;
       this.orthographicCamera.right = (cameraState.distance / 2) * this.aspect;
       this.orthographicCamera.top = cameraState.distance / 2;
@@ -678,8 +669,8 @@ export class Renderer extends EventEmitter<RendererEvents> {
     return {
       perspective: this.config.cameraState.perspective,
       distance: this.controls.getDistance(),
-      phi: this.controls.getPolarAngle(),
-      thetaOffset: -this.controls.getAzimuthalAngle(),
+      phi: THREE.MathUtils.radToDeg(this.controls.getPolarAngle()),
+      thetaOffset: THREE.MathUtils.radToDeg(-this.controls.getAzimuthalAngle()),
       targetOffset: [this.controls.target.x, this.controls.target.y, this.controls.target.z],
       target: this.config.cameraState.target,
       targetOrientation: this.config.cameraState.targetOrientation,
@@ -851,13 +842,14 @@ export class Renderer extends EventEmitter<RendererEvents> {
   }
 
   frameHandler = (currentTime: bigint): void => {
-    const camera = this.activeCamera();
-    this.emit("startFrame", currentTime, this);
-
+    this.currentTime = currentTime;
     this._updateFrames();
     this._updateResolution();
 
     this.gl.clear();
+    this.emit("startFrame", currentTime, this);
+
+    const camera = this.activeCamera();
     camera.layers.set(LAYER_DEFAULT);
     this.selectionBackdrop.visible = this.selectedRenderable != undefined;
 
@@ -1080,14 +1072,15 @@ export class Renderer extends EventEmitter<RendererEvents> {
       this.settings.errors.remove(FOLLOW_TF_PATH, FRAME_NOT_FOUND);
     }
 
-    const rootFrameId = frame.root().id;
-    if (this.fixedFrameId !== rootFrameId) {
+    const fixedFrame = frame.root();
+    const fixedFrameId = fixedFrame.id;
+    if (this.fixedFrameId !== fixedFrameId) {
       if (this.fixedFrameId == undefined) {
-        log.debug(`Setting fixed frame to ${rootFrameId}`);
+        log.debug(`Setting fixed frame to ${fixedFrameId}`);
       } else {
-        log.debug(`Changing fixed frame from "${this.fixedFrameId}" to "${rootFrameId}"`);
+        log.debug(`Changing fixed frame from "${this.fixedFrameId}" to "${fixedFrameId}"`);
       }
-      this.fixedFrameId = rootFrameId;
+      this.fixedFrameId = fixedFrameId;
     }
 
     this.settings.errors.clearPath(FOLLOW_TF_PATH);

packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
@@ -21,7 +21,6 @@ import { DeepPartial } from "ts-essentials";
 import { useDebouncedCallback } from "use-debounce";
 
 import Logger from "@foxglove/log";
-import { CameraState, DEFAULT_CAMERA_STATE, MouseEventObject } from "@foxglove/regl-worldview";
 import { definitions as commonDefs } from "@foxglove/rosmsg-msgs-common";
 import { fromDate, toNanoSec } from "@foxglove/rostime";
 import {
@@ -47,6 +46,7 @@ import type { Renderable } from "./Renderable";
 import { MessageHandler, Renderer, RendererConfig } from "./Renderer";
 import { RendererContext, useRenderer, useRendererEvent } from "./RendererContext";
 import { Stats } from "./Stats";
+import { CameraState, DEFAULT_CAMERA_STATE, MouseEventObject } from "./camera";
 import { FRAME_TRANSFORM_DATATYPES } from "./foxglove";
 import { PublishClickEvent, PublishClickType } from "./renderables/PublishClickTool";
 import { TF_DATATYPES, TRANSFORM_STAMPED_DATATYPES } from "./ros";

packages/studio-base/src/panels/ThreeDeeRender/renderables/CoreSettings.ts
@@ -4,12 +4,12 @@
 
 import { cloneDeep, round, set } from "lodash";
 
-import { DEFAULT_CAMERA_STATE } from "@foxglove/regl-worldview";
 import { SettingsTreeAction } from "@foxglove/studio";
 
 import { Renderer, RendererConfig } from "../Renderer";
 import { SceneExtension } from "../SceneExtension";
 import { SettingsTreeEntry } from "../SettingsManager";
+import { DEFAULT_CAMERA_STATE } from "../camera";
 import { PRECISION_DEGREES, PRECISION_DISTANCE, SelectEntry } from "../settings";
 import { CoordinateFrame } from "../transforms";
 import { PublishClickType } from "./PublishClickTool";

packages/studio-base/src/panels/ThreeDeeRender/Interactions/InteractionContextMenu.tsx
@@ -14,11 +14,11 @@
 import { useCallback } from "react";
 import styled from "styled-components";
 
-import { MouseEventObject } from "@foxglove/regl-worldview";
 import { getObject } from "@foxglove/studio-base/panels/ThreeDimensionalViz/threeDimensionalVizUtils";
 import { BaseMarker } from "@foxglove/studio-base/types/Messages";
 import { colors } from "@foxglove/studio-base/util/sharedStyleConstants";
 
+import { MouseEventObject } from "../camera";
 import { Interactive, SelectedObject } from "./types";
 
 type ClickedPosition = { clientX: number; clientY: number };

packages/studio-base/src/panels/ThreeDeeRender/camera.ts
@@ -1,0 +1,43 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import type { ColorRGBA, Vector3 } from "./ros";
+import type { Pose } from "./transforms";
+
+export type BaseShape = {
+  pose: Pose;
+  scale: Vector3;
+  color?: ColorRGBA;
+};
+
+export type MouseEventObject = {
+  object: BaseShape;
+  instanceIndex?: number;
+};
+
+export type CameraState = {
+  distance: number;
+  perspective: boolean;
+  phi: number;
+  target: readonly [number, number, number];
+  targetOffset: readonly [number, number, number];
+  targetOrientation: readonly [number, number, number, number];
+  thetaOffset: number;
+  fovy: number;
+  near: number;
+  far: number;
+};
+
+export const DEFAULT_CAMERA_STATE: CameraState = {
+  distance: 75,
+  perspective: true,
+  phi: 45,
+  target: [0, 0, 0],
+  targetOffset: [0, 0, 0],
+  targetOrientation: [0, 0, 0, 1],
+  thetaOffset: 0,
+  fovy: 45,
+  near: 0.01,
+  far: 5000,
+};

packages/studio-base/src/panels/ThreeDeeRender/renderables/Urdfs.ts
@@ -6,7 +6,6 @@ import { maxBy } from "lodash";
 
 import { UrdfGeometryMesh, UrdfRobot, UrdfVisual, parseRobot } from "@foxglove/den/urdf";
 import Logger from "@foxglove/log";
-import { Pose } from "@foxglove/regl-worldview";
 import { SettingsTreeAction, SettingsTreeFields } from "@foxglove/studio";
 import { eulerToQuaternion } from "@foxglove/studio-base/util/geometry";
 import isDesktopApp from "@foxglove/studio-base/util/isDesktopApp";
@@ -17,7 +16,7 @@ import { PartialMessageEvent, SceneExtension } from "../SceneExtension";
 import { SettingsTreeEntry } from "../SettingsManager";
 import { ColorRGBA, Marker, MarkerAction, MarkerType, Quaternion, Vector3 } from "../ros";
 import { BaseSettings, CustomLayerSettings } from "../settings";
-import { makePose } from "../transforms";
+import { Pose, makePose } from "../transforms";
 import { updatePose } from "../updatePose";
 import { RenderableCube } from "./markers/RenderableCube";
 import { RenderableCylinder } from "./markers/RenderableCylinder";

packages/studio-base/src/panels/ThreeDeeRender/stories/ArrowMarkers.stories.tsx
@@ -12,6 +12,7 @@ import {
   FIXED_FRAME_ID,
   makeColor,
   QUAT_IDENTITY,
+  rad2deg,
   SENSOR_FRAME_ID,
 } from "./common";
 import useDelayedFixture from "./useDelayedFixture";
@@ -143,10 +144,10 @@ export function ArrowMarkers(): JSX.Element {
           cameraState: {
             distance: 4,
             perspective: true,
-            phi: 1,
+            phi: rad2deg(1),
             targetOffset: [-0.6, 0.5, 0],
-            thetaOffset: -1,
-            fovy: 0.75,
+            thetaOffset: rad2deg(-1),
+            fovy: rad2deg(0.75),
             near: 0.01,
             far: 5000,
             target: [0, 0, 0],

packages/studio-base/src/panels/ThreeDeeRender/stories/AutoSelectFrame.stories.tsx
@@ -12,6 +12,7 @@ import {
   FIXED_FRAME_ID,
   makeColor,
   QUAT_IDENTITY,
+  rad2deg,
   SENSOR_FRAME_ID,
 } from "./common";
 import useDelayedFixture from "./useDelayedFixture";
@@ -97,10 +98,10 @@ export function AutoSelectFrame(): JSX.Element {
           cameraState: {
             distance: 4,
             perspective: true,
-            phi: 1,
+            phi: rad2deg(1),
             targetOffset: [-0.6, 0.5, 0],
-            thetaOffset: -1,
-            fovy: 0.75,
+            thetaOffset: rad2deg(-1),
+            fovy: rad2deg(0.75),
             near: 0.01,
             far: 5000,
             target: [0, 0, 0],

coscene-io/honeybee

commit 056b99566ae046c76572ccec12d56c80bd5aab3b

[New3D] Update settings sidebar when toggling 2D/3D mode via panel (#4157)
**User-Facing Changes**
Fixed a bug in the 3D (Beta) panel where the settings sidebar would not update when toggling between 2D/3D mode via the panel button or keyboard shortcut.
Fixed an issue with numeric step buttons in 3D (Beta) panel settings not working for Phi, Theta, and Y-Axis FOV.

**Description**
For performance reasons, the New3D panel does not call `updatePanelSettingsEditor` any time its configuration changes, but only when the panel deems necessary (usually in a settings action handler, or when a scene extension notices a change in the underlying data source, after a debounced waiting period). The 2D/3D toggle button & keyboard shortcut were implemented via directly changing the config, and the config change triggers a re-render via useEffect, but that did not trigger an update of the settings tree. (The CoreSettings object updates its settings subtree when it detects a cameraMove event from the panel, but in this case the change did not come from the panel.) The fix is to manually tell the CoreSettings object to update the tree after the setting is toggled.

The `ONE_DEGREE` -> `1` change fixes a regression from https://github.com/foxglove/studio/pull/4043 where we forgot to update the step values in settings tree when the values changed from radians to degrees.

packages/studio-base/src/panels/ThreeDeeRender/Renderer.ts
@@ -233,6 +233,7 @@ export class Renderer extends EventEmitter<RendererEvents> {
   input: Input;
   outlineMaterial = new THREE.LineBasicMaterial({ dithering: true });
 
+  coreSettings: CoreSettings;
   measurementTool: MeasurementTool;
   publishClickTool: PublishClickTool;
 
@@ -369,8 +370,9 @@ export class Renderer extends EventEmitter<RendererEvents> {
 
     this.measurementTool = new MeasurementTool(this);
     this.publishClickTool = new PublishClickTool(this);
+    this.coreSettings = new CoreSettings(this);
 
-    this.addSceneExtension(new CoreSettings(this));
+    this.addSceneExtension(this.coreSettings);
     this.addSceneExtension(new Cameras(this));
     this.addSceneExtension(new FrameAxes(this));
     this.addSceneExtension(new Grids(this));
@@ -451,6 +453,11 @@ export class Renderer extends EventEmitter<RendererEvents> {
   updateConfig(updateHandler: (draft: RendererConfig) => void): void {
     this.config = produce(this.config, updateHandler);
     this.emit("configChange", this);
+  }
+
+  /** Updates the settings tree for core settings to account for any changes in the config. */
+  updateCoreSettings(): void {
+    this.coreSettings.updateSettingsTree();
   }
 
   addDatatypeSubscriptions<T>(

packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
@@ -421,7 +421,8 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
     });
   }, [actionHandler, context, settingsTree]);
 
-  // Update the renderer's reference to `config` when it changes
+  // Update the renderer's reference to `config` when it changes. Note that this does *not*
+  // automatically update the settings tree.
   useEffect(() => {
     if (renderer) {
       renderer.config = config;
@@ -756,7 +757,9 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
       ...prevConfig,
       cameraState: { ...prevConfig.cameraState, perspective: !prevConfig.cameraState.perspective },
     }));
-  }, []);
+    // Wait for the setConfig to propagate to the renderer before updating the settings tree
+    setTimeout(() => renderer?.updateCoreSettings(), 0);
+  }, [renderer]);
 
   const onKeyDown = useCallback(
     (event: React.KeyboardEvent) => {

packages/studio-base/src/panels/ThreeDeeRender/renderables/CoreSettings.ts
@@ -30,8 +30,6 @@ export const DEFAULT_PUBLISH_SETTINGS: RendererConfig["publish"] = {
   poseEstimateThetaDeviation: round(Math.PI / 12, 8),
 };
 
-const ONE_DEGREE = Math.PI / 180;
-
 export class CoreSettings extends SceneExtension {
   constructor(renderer: Renderer) {
     super("foxglove.CoreSettings", renderer);
@@ -145,7 +143,7 @@ export class CoreSettings extends SceneExtension {
                 thetaOffset: {
                   label: "Theta",
                   input: "number",
-                  step: ONE_DEGREE,
+                  step: 1,
                   precision: PRECISION_DEGREES,
                   value: camera.thetaOffset,
                 },
@@ -153,14 +151,14 @@ export class CoreSettings extends SceneExtension {
                   phi: {
                     label: "Phi",
                     input: "number",
-                    step: ONE_DEGREE,
+                    step: 1,
                     precision: PRECISION_DEGREES,
                     value: camera.phi,
                   },
                   fovy: {
                     label: "Y-Axis FOV",
                     input: "number",
-                    step: ONE_DEGREE,
+                    step: 1,
                     precision: PRECISION_DEGREES,
                     value: camera.fovy,
                   },