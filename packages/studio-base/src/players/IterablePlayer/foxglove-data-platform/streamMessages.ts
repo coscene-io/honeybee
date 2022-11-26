407deaa64aa5bc44e9830a83f07fc80f2bc3f1be
Woodii <junhui.li@coscene.io>
2022-11-26 15:21:11 +0800

    Major fixup.

--- packages/studio-base/src/players/IterablePlayer/foxglove-data-platform/streamMessages.ts
+++ packages/studio-base/src/players/IterablePlayer/foxglove-data-platform/streamMessages.ts
@@ -189,6 +189,7 @@ export async function* streamMessages({
           // Include the version of studio in the request Useful when scraping logs to determine what
           // versions of the app are making requests.
           "fg-user-agent": FOXGLOVE_USER_AGENT,
+          Authorization: `Bearer ${localStorage.getItem("coScene_org_jwt")}`,
         },
       },
     );
