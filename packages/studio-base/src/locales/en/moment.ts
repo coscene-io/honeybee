2b07d9637c1580911810f581d9053ffe902b3c62
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-02-07 19:31:04 +0800

    update: moment Translation (#88)

---
+++ packages/studio-base/src/locales/en/moment.ts
@@ -0,0 +1,11 @@
+export default {
+  createMoment: "Create Moment",
+  startTime: "Start Time",
+  duration: "Duration",
+  description: "Description",
+  metadata: "Metadata",
+  key: "Key",
+  value: "Value",
+  string: "String",
+  sec: "sec",
+};
