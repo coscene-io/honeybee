1401bbc6e2b11beccd863297be364f793608894a
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-02-20 20:36:09 +0800

    update: Optimization recenter and zoom (#114)

--- packages/studio/index.d.ts
+++ packages/studio/index.d.ts
@@ -2,8 +2,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-// import addPanel from "@foxglove/studio-base/locales/en/addPanel";
-
 declare module "@foxglove/studio" {
   // Valid types for parameter data (such as rosparams)
   export type ParameterValue =
