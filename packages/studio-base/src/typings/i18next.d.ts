e682528c4a7978353f6ecf8def69bae99a74e496
Woodii <junhui.li@coscene.io>
2023-03-08 10:44:10 +0800

    it is hump day _^_

--- packages/studio-base/src/typings/i18next.d.ts
+++ packages/studio-base/src/typings/i18next.d.ts
@@ -8,6 +8,6 @@ declare module "i18next" {
   interface CustomTypeOptions {
     returnNull: false;
     defaultNS: typeof defaultNS;
-    resources: typeof translations["en"];
+    resources: (typeof translations)["en"];
   }
 }
