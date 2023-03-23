10ed3aadef259dfd790f9d6fdb276d1a54954341
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-03-23 14:42:02 +0800

    ID:10T Error

--- packages/studio-base/src/test/mocks/MockCss.ts
+++ packages/studio-base/src/test/mocks/MockCss.ts
@@ -2,8 +2,4 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-<<<<<<<< HEAD:packages/studio-web/public/cos-config.js
-window.cosConfig = {};
-========
 export {};
->>>>>>>> release/v1.45.0:packages/studio-base/src/test/mocks/MockCss.ts
