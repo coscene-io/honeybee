5313cfebd6e3a925d96e6987896385ebf1c67ecf
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-05-15 17:45:05 +0800

    feat: add quality options (#174)

---
+++ packages/studio-base/src/i18n/cosZh/settings.ts
@@ -0,0 +1,13 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+import { TypeOptions } from "i18next";
+
+export const cosSettings: TypeOptions["resources"]["cosSettings"] = {
+  high: "高",
+  medium: "中",
+  low: "低",
+  original: "原始值",
+  quality: "画质",
+  willTakeEffectOnTheNextStartup: "将在下次启动时生效",
+};
