475029f2acf33e311908a5ee3a45929cd3a6549a
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-05-15 17:45:05 +0800

    feat: add quality options (#174)

---
+++ packages/studio-base/src/i18n/cosEn/settings.ts
@@ -0,0 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+export const cosSettings = {
+  high: "High",
+  medium: "Medium",
+  low: "Low",
+  original: "Original",
+  quality: "Quality",
+  willTakeEffectOnTheNextStartup: "Will take effect on the next startup",
+};
