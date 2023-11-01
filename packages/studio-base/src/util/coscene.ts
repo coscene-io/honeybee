5e856bc9bec9dc0c39358f874afbce536d15ce9d
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-11-01 11:33:29 +0800

    update: sync language (#224)

--- packages/studio-base/src/util/coscene.ts
+++ packages/studio-base/src/util/coscene.ts
@@ -3,6 +3,16 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 // coScene custom tools
+import { createPromiseClient, PromiseClient, Interceptor } from "@bufbuild/connect";
+import { createGrpcWebTransport } from "@bufbuild/connect-web";
+import { ServiceType } from "@bufbuild/protobuf";
+import {
+  ACCESS_TOKEN_NAME,
+  SUPER_TOKEN_ACCESS_TOKEN_NAME,
+  uuidv4,
+} from "@coscene-io/coscene/queries";
+import { StatusCode } from "grpc-web";
+import i18next from "i18next";
 
 export function getPlaybackQualityLevelByLocalStorage(): "ORIGINAL" | "HIGH" | "MID" | "LOW" {
   const localPlaybackQualityLevel = localStorage.getItem("playbackQualityLevel");
@@ -50,4 +60,62 @@ export function getOS(): string | undefined {
   }
 
   return os;
+}
+
+const setAuthorizationUnaryInterceptor: Interceptor = (next) => async (req) => {
+  const jwt =
+    localStorage.getItem(ACCESS_TOKEN_NAME) ?? localStorage.getItem(SUPER_TOKEN_ACCESS_TOKEN_NAME);
+  if (jwt) {
+    req.header.set("Authorization", jwt);
+    req.header.set("x-cos-request-id", uuidv4());
+  } else {
+    console.error("no jwt");
+  }
+
+  try {
+    return await next(req);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    // grpc error code-16 === http status code 401
+    // https://grpc.github.io/grpc/core/md_doc_statuscodes.html
+    if (error.code === StatusCode.UNAUTHENTICATED) {
+      localStorage.removeItem("demoSite");
+      localStorage.removeItem("joyrideInfoList");
+      localStorage.removeItem("honeybeeDemoStatus");
+      if (window.location.pathname !== "/login") {
+        window.location.href = `/login?redirectToPath=${encodeURIComponent(
+          window.location.pathname + window.location.search,
+        )}`;
+      }
+    }
+
+    console.error(error);
+    try {
+      console.error(
+        `Error Calling GRPC Service ${req.service.typeName} Code: ${error.code} Message: ${error.message}`,
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return await next(req);
+};
+
+const setLocaleInfoUnaryInterceptor: Interceptor = (next) => async (req) => {
+  req.header.set("x-cos-utc-offset", String(new Date().getTimezoneOffset()));
+  req.header.set("x-cos-language-code", i18next.language);
+
+  return await next(req);
+};
+
+export function getPromiseClient<T extends ServiceType>(service: T): PromiseClient<T> {
+  return createPromiseClient(
+    service,
+    createGrpcWebTransport({
+      baseUrl: window.cosConfig.VITE_APP_BASE_API_URL ?? "",
+      interceptors: [setAuthorizationUnaryInterceptor, setLocaleInfoUnaryInterceptor],
+    }),
+  );
 }
