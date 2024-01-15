7befbfb2aecb132904077fc69d88af4e84b6def6
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-01-15 15:48:56 +0800

    I hate this fucking language.

--- packages/studio-base/src/services/LayoutManager/CoSceneLayoutManager.ts
+++ packages/studio-base/src/services/LayoutManager/CoSceneLayoutManager.ts
@@ -316,7 +316,7 @@ export default class CoSceneLayoutManager implements ILayoutManager {
             syncInfo: { status: "tracked", lastRemoteSavedAt: updatedBaseline.savedAt },
           }),
       );
-      // 当 busyCount > 1 时，代表着有多个更新 layout 的任务在排队，这时不应该出发 change 事件，否则会导致 layout 跳回上一个版本
+      // 当 busyCount > 1 时，代表着有多个更新 layout 的任务在排队，这时不应该触发 change 事件，否则会导致 layout 跳回上一个版本
       if (this.#busyCount === 1) {
         this.#notifyChangeListeners({ type: "change", updatedLayout: result });
       }
