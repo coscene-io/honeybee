1517444bc650c95f502d1d28ab1e66f6bf8d6733
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-01-17 18:56:47 +0800

    update: catch coScene error

---
+++ packages/studio-base/src/CoSceneErrors.ts
@@ -0,0 +1,22 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+export const CoSceneErrors: { [key: number]: string } = {
+  1000: "getFilesFromLocalCacheError",
+  1001: "revisionIsNull",
+  1002: "getFilesFromDpsError",
+  1003: "noFileToRead",
+  1004: "illegalRequestTimestamp",
+  1005: "illegalArgument",
+  1006: "semanticLibError",
+  1007: "parseMediaFileFailed",
+  1008: "parseDataInterpretationFailed",
+  1009: "blankAuthToken",
+  1010: "invalidToken",
+  1011: "permissionDenied",
+  1012: "unknownAuthError",
+  1013: "generateMediaFailed",
+  9998: "useForAlwaysFail",
+  9999: "unknownError",
+};
--- packages/studio-base/src/i18n/cosEn/error.ts
+++ packages/studio-base/src/i18n/cosEn/error.ts
@@ -4,4 +4,20 @@
 
 export const cosError = {
   loginExpired: "Login expired, please login again",
+  getFilesFromLocalCacheError: "Failed to retrieve files from local cache",
+  revisionIsNull: "Revision is null",
+  getFilesFromDpsError: "Failed to retrieve files from DPS (Data Processing System)",
+  noFileToRead: "No file to read",
+  illegalRequestTimestamp: "Illegal request timestamp",
+  illegalArgument: "Illegal argument",
+  semanticLibError: "SemanticLib threw an exception",
+  parseMediaFileFailed: "Failed to parse media file",
+  parseDataInterpretationFailed: "Failed to parse data interpretation",
+  blankAuthToken: "Blank auth token in the request",
+  invalidToken: "Invalid token",
+  permissionDenied: "Permission denied to perform the requested action",
+  unknownAuthError: "Unknown authentication error",
+  generateMediaFailed: "Failed to generate media",
+  useForAlwaysFail: "Test interface for always failing",
+  unknownError: "Unknown error",
 };
