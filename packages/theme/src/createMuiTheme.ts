6494e6355f217455fb3d0baba5c8522f9a847308
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-01-29 16:23:52 +0800

    update: annotated plot i18n

--- packages/theme/src/createMuiTheme.ts
+++ packages/theme/src/createMuiTheme.ts
@@ -2,6 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import * as locales from "@mui/material/locale";
 import { createTheme, Theme } from "@mui/material/styles";
 
 import * as components from "./components";
@@ -19,11 +20,27 @@ declare module "@mui/material/styles" {
   }
 }
 
-export const createMuiTheme = (themePreference: ThemePreference): Theme =>
-  createTheme({
-    name: themePreference,
-    palette: palette[themePreference],
-    shape: { borderRadius: 2 },
-    typography,
-    components,
-  });
+export function i18nextLanguageToMuiImportName(lang: string): locales.Localization {
+  switch (lang) {
+    case "zh":
+      return locales["zhCN"];
+    case "jp":
+      return locales["jaJP"];
+    case "en":
+      return locales["enUS"];
+    default:
+      return locales["enUS"];
+  }
+}
+
+export const createMuiTheme = (themePreference: ThemePreference, lang: string): Theme =>
+  createTheme(
+    {
+      name: themePreference,
+      palette: palette[themePreference],
+      shape: { borderRadius: 2 },
+      typography,
+      components,
+    },
+    i18nextLanguageToMuiImportName(lang),
+  );
