61e4e4a163549d3c5fcbddb612590a27d1b3bb7f
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-01-30 11:22:14 +0800

    pref: color mapping func (#270)

--- packages/studio-base/src/util/coscene/index.ts
+++ packages/studio-base/src/util/coscene/index.ts
@@ -191,9 +191,11 @@ export function stringToColor(str: string): string {
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
+
   let color = "#";
   for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff;
+    let value = (hash >> (i * 8)) & 0xff;
+    value = Math.floor(value * 0.3 + 0.5 * 0xff); // adjust value to get a brightness between 30% and 50%
     color += ("00" + value.toString(16)).substr(-2);
   }
   return color;
