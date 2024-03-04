3df2c4f6aea4db0d1144f5a49b3b89ae22d44483
Qi He <heqi37@gmail.com>
2024-03-04 14:49:09 +0800

    feat: download plot data as CSV i18n (#301)

--- packages/studio-base/src/i18n/cosZh/annotatedPlot.ts
+++ packages/studio-base/src/i18n/cosZh/annotatedPlot.ts
@@ -4,15 +4,16 @@
 import { TypeOptions } from "i18next";
 
 export const cosAnnotatedPlot: Partial<TypeOptions["resources"]["cosAnnotatedPlot"]> = {
-  name: "名称",
   annotatedPlot: "标注图表",
+  display: "显示",
+  downloadPlotDataAsCSV: "下载图表数据为CSV",
+  messageLabel: "消息标签",
+  name: "名称",
+  plaseEnterName: "请输入名称",
   prefixTip: "要使用标注图表组件，请在设置页面上打开添加主题前缀选项",
+  resetView: "重置视图",
   y1Axis: "第二Y轴",
   y2Axis: "第三Y轴",
   y3Axis: "第四Y轴",
   y4Axis: "第五Y轴",
-  display: "显示",
-  plaseEnterName: "请输入名称",
-  resetView: "重置视图",
-  messageLabel: "消息标签",
 };
