172f0b3e210742a759415c456f9f8a5c5e2c2608
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-03-05 20:43:40 +0800

    fix: i18n

--- packages/studio-base/src/i18n/cosEn/layout.ts
+++ packages/studio-base/src/i18n/cosEn/layout.ts
@@ -63,4 +63,6 @@ export const cosLayout = {
   update: "Update",
   updateRemoteLayoutConfirm:
     "Your changes will overwrite this layout for all organization members. This cannot be undone.",
+  notLayoutDesc: "You don't currently have a layout selected.",
+  createNewLayout: "Create a new layout",
 };
