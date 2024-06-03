60f9fcc07d53b07abaf16b85ff88b2c66080bba2
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-06-03 16:06:25 +0800

    pref: create moment

--- packages/studio-web/public/cos-config.js
+++ packages/studio-web/public/cos-config.js
@@ -2,4 +2,29 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-window.cosConfig = {};
+// Basic YXBpa2V5Ok9HTTNaV0ZsT0RjNE1EQm1Oamt4TVdVellqWmxZemsyWkRZM05EZzNZMlJtTW1RMU9USTBNREF3WWpjNU5ESmpPVEl3Wm1JM1pUYzRaVFpsT0RWak1RPT0=
+window.cosConfig = {
+  CS_HONEYBEE_BASE_URL: "https://honeybee.staging.coscene.cn",
+  GROWTHBOOK_API_HOST: "https://ffproxy.coscene.site",
+  GROWTHBOOK_CLIENT_KEY: "sdk-2qPrrGJlttAbLBuH",
+  LAUNCHDARKLY_CLIENT_SIDE_ID: "646c2f413ff9a31232693f7e",
+  LAYOUT_TEMPLATE_INDEX_OSS_URL:
+    "https://coscene-artifacts-prod.oss-cn-hangzhou.aliyuncs.com/honeybee_layouts/staging/index.json",
+  LOGO_CONFIG: {
+    "staging.coscene.cn": {
+      logo: "coscene",
+      sso_domain: "https://sso.staging.coscene.cn",
+    },
+    "web.staging.coscene.cn": {
+      logo: "supor",
+      sso_domain: "https://web.sso.staging.coscene.cn",
+    },
+  },
+  SENTRY_AUTH_TOKEN: "5d7fd483a3c5412f80bc5ce61fcad735868109e4c221498f97c34c39fac24e12",
+  SENTRY_ENABLED: true,
+  SENTRY_HONEYBEE_DSN: "https://57e1ac8cf30c4705a2fd73e8f396c05e@sentry.coscene.site/5",
+  VITE_APP_BASE_API_PORT: 443,
+  VITE_APP_BASE_API_URL: "https://api.staging.coscene.cn",
+  VITE_APP_BFF_URL: "https://bff.staging.coscene.cn",
+  VITE_APP_PROJECT_ENV: "staging",
+};
