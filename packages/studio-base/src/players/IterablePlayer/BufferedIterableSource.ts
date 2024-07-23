6493f4a3f76a80d24fc6894d90ea77e055578b8c
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-07-23 17:30:17 +0800

    Hanlle rebase plx?

--- packages/studio-base/src/players/IterablePlayer/BufferedIterableSource.ts
+++ packages/studio-base/src/players/IterablePlayer/BufferedIterableSource.ts
@@ -228,6 +228,7 @@ class BufferedIterableSource<MessageType = unknown>
   }
 
   public async stopProducer(): Promise<void> {
+    log.debug("Stopping producer");
     this.#aborted = true;
     this.#writeSignal.notifyAll();
     await this.#producer;
