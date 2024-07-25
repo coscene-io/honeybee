b345acc15292f7ab42dbce624c2b19353f96d094
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-07-25 18:05:27 +0800

    [COS-6305] replace: foxglove text to coscene (#483)

--- packages/studio-base/src/i18n/ja/general.ts
+++ packages/studio-base/src/i18n/ja/general.ts
@@ -6,9 +6,9 @@ import { TypeOptions } from "i18next";
 
 // Generic global translation
 export const general: Partial<TypeOptions["resources"]["general"]> = {
-  // Note that we use the term `Foxglove` as an English word rather than a Japanese
+  // Note that we use the term `CoScene` as an English word rather than a Japanese
   // word since it would sound very different - フォックスグローブ (Fokusugorobu).
-  foxglove: "Foxglove",
+  foxglove: "CoScene",
   learnMore: "もっと学ぶ",
   on: undefined,
   off: undefined,
