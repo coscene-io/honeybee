67399f3009075cbf1f8dc6c4c532943802d7d2f6
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-07-30 21:16:10 +0800

    update: move h264 decoder to worker (#493)

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
@@ -4,10 +4,17 @@
 
 import * as Comlink from "comlink";
 
+import Logger from "@foxglove/log";
 import type { RawImage } from "@foxglove/schemas";
 
 import { decodeRawImage, RawImageOptions } from "./decodeImage";
 import type { Image as RosImage } from "../../ros";
+
+const log = Logger.getLogger(__filename);
+
+let h264Decoder: VideoDecoder | undefined;
+
+let H264Frames: VideoFrame[] = [];
 
 function decode(image: RosImage | RawImage, options: Partial<RawImageOptions>): ImageData {
   const result = new ImageData(image.width, image.height);
@@ -15,7 +22,88 @@ function decode(image: RosImage | RawImage, options: Partial<RawImageOptions>): 
   return Comlink.transfer(result, [result.data.buffer]);
 }
 
+function isKeyFrame(uint8Array: Uint8Array): boolean {
+  let naltype: string = "invalid frame";
+  // 查找 NAL 单元起始码
+  if (uint8Array.length > 4) {
+    if (uint8Array[4] === 0x65) {
+      naltype = "I frame";
+    } else if (uint8Array[4] === 0x41) {
+      naltype = "P frame";
+    } else if (uint8Array[4] === 0x67) {
+      naltype = "SPS";
+    } else if (uint8Array[4] === 0x68) {
+      naltype = "PPS";
+    }
+  }
+
+  // 如果没有找到有效的 NAL 单元，返回 false
+  if (naltype === "I frame" || naltype === "SPS" || naltype === "PPS") {
+    return true;
+  }
+  return false;
+}
+
+function getH264Decoder(): VideoDecoder {
+  if (!h264Decoder) {
+    h264Decoder = new VideoDecoder({
+      output: (frame: VideoFrame) => {
+        H264Frames.push(frame);
+      },
+      error: (error: Error) => {
+        log.error(error.message);
+      },
+    });
+
+    h264Decoder.configure({
+      codec: "avc1.42001E",
+      hardwareAcceleration: "prefer-hardware",
+    });
+  }
+  return h264Decoder;
+}
+
+function decodeH264Frame(data: Uint8Array | Int8Array, sequenceNumber: number): void {
+  let type: "delta" | "key" = "delta";
+  if (data.length > 4) {
+    if (isKeyFrame(data as Uint8Array)) {
+      type = "key";
+    } else {
+      type = "delta";
+    }
+  }
+
+  const chunk = new EncodedVideoChunk({
+    timestamp: sequenceNumber,
+    type,
+    data,
+  });
+
+  const decoder = getH264Decoder();
+
+  try {
+    decoder.decode(chunk);
+  } catch (error) {
+    log.error(error);
+  }
+}
+
+async function getH264Frames(): Promise<ImageBitmap | undefined> {
+  const frame = H264Frames.pop();
+  if (frame) {
+    const imageBitmap = await createImageBitmap(frame);
+    return imageBitmap;
+  }
+  H264Frames.forEach((uselessFrame) => {
+    uselessFrame.close();
+  });
+  H264Frames = [];
+  return undefined;
+}
+
 export const service = {
   decode,
+  decodeH264Frame,
+  getH264Frames,
 };
 Comlink.expose(service);
