def774b5f01a377ff504465da4d12b01569961a5
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-08-02 18:34:48 +0800

    update: filter h264 b frame

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
@@ -22,9 +22,22 @@ function decode(image: RosImage | RawImage, options: Partial<RawImageOptions>): 
   return Comlink.transfer(result, [result.data.buffer]);
 }
 
-function isKeyFrame(frame: Uint8Array): boolean {
+function convertToBinaryArray(uint8Array: Uint8Array) {
+  return uint8Array.reduce((acc: number[], num) => {
+    // 将每个数字转换为8位二进制字符串
+    const binaryString = num.toString(2).padStart(8, "0");
+
+    // 将二进制字符串分割成单个位，并转换为数字
+    const binaryDigits = binaryString.split("").map(Number);
+
+    // 将转换后的二进制数字添加到结果数组中
+    return acc.concat(binaryDigits);
+  }, []);
+}
+
+function isKeyFrame(frame: Uint8Array): "key" | "error frame" | "delta" {
   if (frame.length < 5) {
-    return false; // 帧太短，无法判断
+    return "error frame"; // 帧太短，无法判断
   }
 
   // 查找 NAL 单元的起始位置
@@ -36,7 +49,7 @@ function isKeyFrame(frame: Uint8Array): boolean {
   }
 
   if (nalStart >= frame.length) {
-    return false; // NAL 单元起始位置无效
+    return "error frame"; // NAL 单元起始位置无效
   }
 
   // 获取 NAL 单元类型
@@ -44,7 +57,77 @@ function isKeyFrame(frame: Uint8Array): boolean {
 
   // IDR 帧 (type 5) 始终是关键帧
   // SPS (type 7) 和 PPS (type 8) 也被视为关键帧，因为它们对解码至关重要
-  return nalType === 5 || nalType === 7 || nalType === 8;
+  if (nalType === 5 || nalType === 7 || nalType === 8) {
+    return "key";
+  }
+
+  // 00 00 01 65 01 92 22
+  // 0110 0101 0000 0001 1001 0010 0010 0010
+  // forbidden_zero_bit:
+  // 0
+  // nal_ref_idc:
+  // 11
+  // nal_unit_type:
+  // 00101
+  // first_mb_in_slice:
+  // 0000 000 1 1001 001 -> 2^7 -1 + 73 = 200
+  // slice_type:
+  // 0 0010 00 -> 2^3 -1 + 0 = 7
+  // first_mb_in_slice start
+
+  // 跳过 forbidden_zero_bit 和 nal_ref_idc 以及 nal_unit_type 正好是八位二进制 跳过第一个 数字
+  const first_mb_in_slice = nalStart + 1;
+
+  if (
+    frame[first_mb_in_slice] == undefined ||
+    frame[first_mb_in_slice + 1] == undefined ||
+    frame[first_mb_in_slice + 2] == undefined
+  ) {
+    return "error frame";
+  }
+
+  //将 first_mb_in_slice 后面三位数转换为 2 进制数组
+  const binaryArray = convertToBinaryArray(
+    new Uint8Array([
+      frame[first_mb_in_slice],
+      frame[first_mb_in_slice + 1] ?? 0,
+      frame[first_mb_in_slice + 2] ?? 0,
+    ]),
+  );
+
+  // 找到第一个 1
+  const firstMbOneIndex = binaryArray.findIndex((value) => value === 1);
+
+  // 1 前面有多少位也跳过 1 后面多少位，找到 slice_type 的起始位
+  const sliceTypeArray = binaryArray.slice(firstMbOneIndex * 2 + 1);
+
+  const firstSliceTypeOneIndex = sliceTypeArray.findIndex((value) => value === 1);
+
+  // 2 ^ firstSliceTypeOneIndex - 1 + (1前有多少位就向后计算多少位的十进制)
+  const sliceType =
+    2 ** firstSliceTypeOneIndex -
+    1 +
+    parseInt(
+      sliceTypeArray.slice(firstSliceTypeOneIndex + 1, 2 * firstSliceTypeOneIndex + 1).join(""),
+      2,
+    );
+
+  // 0: P slice
+  // 1: B slice
+  // 2: I slice
+  // 3: SP slice
+  // 4: SI slice
+  // 5: P slice (IDR)
+  // 6: B slice (IDR)
+  // 7: I slice (IDR)
+  // 8: SP slice (IDR)
+  // 9: SI slice (IDR)
+  // 不支持 b frame
+  if (sliceType === 1 || sliceType === 6) {
+    return "error frame";
+  }
+
+  return "delta";
 }
 
 function getH264Decoder(): VideoDecoder {
@@ -67,13 +150,13 @@ function getH264Decoder(): VideoDecoder {
 }
 
 function decodeH264Frame(data: Uint8Array | Int8Array, sequenceNumber: number): void {
-  let type: "delta" | "key" = "delta";
+  let type: "delta" | "key" | "error frame" = "delta";
   if (data.length > 4) {
-    if (isKeyFrame(data as Uint8Array)) {
-      type = "key";
-    } else {
-      type = "delta";
-    }
+    type = isKeyFrame(data as Uint8Array);
+  }
+
+  if (type === "error frame") {
+    return;
   }
 
   const chunk = new EncodedVideoChunk({
