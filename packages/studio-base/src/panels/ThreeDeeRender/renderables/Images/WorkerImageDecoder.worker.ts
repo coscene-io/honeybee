d0e62666a79e30a69635ef9fbd9d9508a72d8102
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-08-02 10:48:44 +0800

    update: h264 hardwareAcceleration config

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
@@ -60,7 +60,7 @@ function getH264Decoder(): VideoDecoder {
 
     h264Decoder.configure({
       codec: "avc1.42001E",
-      hardwareAcceleration: "prefer-software",
+      optimizeForLatency: true,
     });
   }
   return h264Decoder;
