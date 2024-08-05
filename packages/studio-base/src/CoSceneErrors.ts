d33fb7cae61e9af252f445c19622054bcbb1ed74
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-08-05 17:06:51 +0800

    [COS-5480] update: error code message

--- packages/studio-base/src/CoSceneErrors.ts
+++ packages/studio-base/src/CoSceneErrors.ts
@@ -3,20 +3,48 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 export const CoSceneErrors: { [key: number]: string } = {
-  1000: "getFilesFromLocalCacheError",
-  1001: "revisionIsNull",
-  1002: "getFilesFromDpsError",
-  1003: "noFileToRead",
-  1004: "illegalRequestTimestamp",
-  1005: "illegalArgument",
-  1006: "semanticLibError",
-  1007: "parseMediaFileFailed",
-  1008: "parseDataInterpretationFailed",
-  1009: "blankAuthToken",
-  1010: "invalidToken",
-  1011: "permissionDenied",
-  1012: "unknownAuthError",
-  1013: "generateMediaFailed",
-  9998: "useForAlwaysFail",
-  9999: "unknownError",
+  // semantic-lib 内部错误
+  1000: "SEMANTIC_LIB_ERROR",
+
+  // 从data platform 获取 jobrun 失败
+  1100: "GET_JOBRUN_FROM_DPS",
+  // 从data platform 获取 project 信息失败
+  1101: "GET_PROJECT_FROM_DPS",
+  // 从data platform 获取 Files 失败
+  1102: "GET_FILES_FROM_DPS",
+  // 从data platform 获取 record 失败
+  1103: "GET_RECORD_FROM_DPS",
+
+  // 非法时间戳
+  1200: "ILLEGAL_REQUEST_TIMESTAMP",
+  // 非法参数
+  1201: "ILLEGAL_ARGUMENT",
+
+  // 解析media buffer失败
+  1300: "PARSE_MEDIA_BUFFER_FAILED",
+  // 生成media失败
+  1301: "GENERATE_MEDIA_FAILED",
+  // 生成data interpretation失败
+  1302: "GENERATE_DATA_INTERPRETATION_FAILED",
+  // media 丢失, 正在异步生成, 稍后再试
+  1303: "FILE_MEDIA_LOST",
+  // 读取media文件失败
+  1304: "READ_MEDIA_FILE_FAILED",
+
+  // auth为空
+  1400: "BLANK_AUTH_TOKEN",
+  // token不合法
+  1401: "INVALID_TOKEN",
+  // token被拒绝
+  1402: "TOKEN_PERMISSION_DENIED",
+  // 未知Token错误
+  1403: "UNKNOWN_AUTH_ERROR",
+
+  // 从bff获取信息失败
+  1500: "GET_RESPONSE_FROM_BFF",
+
+  // 仅alwaysFail接口触发该error code
+  9900: "USE_FOR_ALWAYS_FAIL",
+  // 其他错误
+  9901: "UNKNOWN_ERROR",
 };
