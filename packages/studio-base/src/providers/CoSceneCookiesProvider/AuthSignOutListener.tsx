a12cfd83e5bbf5f473163d2e6dcca6e6fd779fe6
Qi He <heqi37@gmail.com>
2024-08-15 14:53:00 +0800

    lint fix

--- packages/studio-base/src/providers/CoSceneCookiesProvider/AuthSignOutListener.tsx
+++ packages/studio-base/src/providers/CoSceneCookiesProvider/AuthSignOutListener.tsx
@@ -2,10 +2,10 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
+import { useEffect } from "react";
+import { useCookies } from "react-cookie";
 
-import { AUTH_STATUS_COOKIE_NAME, AuthStatus } from './constant';
+import { AUTH_STATUS_COOKIE_NAME, AuthStatus } from "./constant";
 
 function AuthSignOutListener(): JSX.Element {
   const [cookies] = useCookies([AUTH_STATUS_COOKIE_NAME]);
@@ -14,9 +14,9 @@ function AuthSignOutListener(): JSX.Element {
 
   useEffect(() => {
     if (signOut) {
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
-  }, [ signOut]);
+  }, [signOut]);
 
   return <></>;
 }
--- packages/studio-base/src/providers/CoSceneCookiesProvider/index.tsx
+++ packages/studio-base/src/providers/CoSceneCookiesProvider/index.tsx
@@ -2,16 +2,20 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { CookiesProvider } from 'react-cookie';
+import { CookiesProvider } from "react-cookie";
 
-import { AuthSignOutListener } from '@foxglove/studio-base/providers/CoSceneCookiesProvider/AuthSignOutListener';
+import { AuthSignOutListener } from "@foxglove/studio-base/providers/CoSceneCookiesProvider/AuthSignOutListener";
 
-import { cookieSetOptions } from './constant';
+import { cookieSetOptions } from "./constant";
 
-export default function CoSceneCookiesProvider({ children }: { children?: React.PropsWithChildren }): JSX.Element {
+export default function CoSceneCookiesProvider({
+  children,
+}: {
+  children?: React.PropsWithChildren;
+}): JSX.Element {
   return (
     <CookiesProvider defaultSetOptions={cookieSetOptions}>
-      <AuthSignOutListener/>
+      <AuthSignOutListener />
       {children}
     </CookiesProvider>
   );
