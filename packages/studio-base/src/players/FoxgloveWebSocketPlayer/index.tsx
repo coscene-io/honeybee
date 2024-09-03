8b15351d40b26d21d1ea364a4b2ae88a985a2aba
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-09-03 19:01:27 +0800

    rename: foxglove bridge to coBridge

--- packages/studio-base/src/players/FoxgloveWebSocketPlayer/index.tsx
+++ packages/studio-base/src/players/FoxgloveWebSocketPlayer/index.tsx
@@ -256,7 +256,7 @@ export default class FoxgloveWebSocketPlayer implements Player {
           message: "Insecure WebSocket connection",
           tip: `Check that the WebSocket server at ${
             this.#url
-          } is reachable and supports protocol version ${FoxgloveClient.SUPPORTED_SUBPROTOCOL}.`,
+          } is reachable and supports protocol version coscene.websocket.protocol.`,
         });
         this.#emitState();
       }
@@ -306,7 +306,7 @@ export default class FoxgloveWebSocketPlayer implements Player {
             <span>
               {`${t("cosError:insecureWebSocketConnectionMessage", {
                 url: this.#url,
-                version: FoxgloveClient.SUPPORTED_SUBPROTOCOL,
+                version: 'coscene.websocket.protocol'
               })}`}
               <br />
               1. {t("cosError:checkNetworkConnection")}
