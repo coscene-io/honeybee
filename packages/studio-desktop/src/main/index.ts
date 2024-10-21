302ce511e9bfe80025a66b230eb2f28e6a7fdb1a
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-10-21 15:52:43 +0800

    fix: electron main process i18n (#608)

--- packages/studio-desktop/src/main/index.ts
+++ packages/studio-desktop/src/main/index.ts
@@ -8,6 +8,7 @@ import i18n from "i18next";
 
 import Logger from "@foxglove/log";
 import { AppSetting } from "@foxglove/studio-base/src/AppSetting";
+import { initI18n } from "@foxglove/studio-base/src/i18n";
 
 import StudioAppUpdater from "./StudioAppUpdater";
 import StudioWindow from "./StudioWindow";
@@ -56,8 +57,8 @@ async function updateLanguage() {
 }
 
 export async function main(): Promise<void> {
-  // await initI18n({ context: "electron-main" });
-  // await updateLanguage();
+  await initI18n({ context: "electron-main" });
+  await updateLanguage();
 
   // Allow integration tests to override the userData directory
   const userDataOverride = process.argv.find((arg) => arg.startsWith("--user-data-dir="));
