a2e0554bf773dc341eeec8a7bed43ab59bb66289
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-10-29 10:11:32 +0800

    refactor: remove Joyride feature and simplify data sync (#616)

--- packages/studio-web/src/CoSceneInit.tsx
+++ packages/studio-web/src/CoSceneInit.tsx
@@ -1,17 +1,43 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useTranslation } from "react-i18next";
 import { useFavicon } from "react-use";
 
+import Logger from "@foxglove/log";
 import { APP_CONFIG, getDomainConfig } from "@foxglove/studio-base/util/appConfig";
 
-export function useCoSceneInit(): void {
-  let favicon = "";
-  const { t } = useTranslation("cosError");
+const log = Logger.getLogger(__filename);
+
+export function useCoSceneInit({ baseUrl, jwt }: { baseUrl: string; jwt: string }): boolean {
+  const [isLoading, setIsLoading] = useState(true);
 
   const url = new URL(window.location.href);
+
+  useEffect(() => {
+    const urlKey = url.searchParams.get("ds.key");
+
+    fetch(`${baseUrl}/v1/data/sync`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: jwt,
+      },
+      body: JSON.stringify({ id: urlKey }),
+    })
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch(() => {
+        log.error("Failed sync data to honeybee server");
+        setIsLoading(false);
+      });
+  }, [baseUrl, jwt, url.searchParams]);
+
+  let favicon = "";
+  const { t } = useTranslation("cosError");
 
   const urlFiles = url.searchParams.get("ds.files");
 
@@ -37,4 +63,6 @@ export function useCoSceneInit(): void {
   }
 
   useFavicon(favicon);
+
+  return isLoading;
 }
