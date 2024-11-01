4fc76b7b28a51e9680557415327731e8e3c62999
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-11-01 11:22:42 +0800

    lots of changes after a lot of time

--- packages/studio-base/src/util/appConfig.ts
+++ packages/studio-base/src/util/appConfig.ts
@@ -41,8 +41,60 @@ declare global {
   }
 }
 
+const defaultCnConfig = {
+  CS_HONEYBEE_BASE_URL: "https://honeybee.coscene.cn",
+  DOMAIN_CONFIG: {
+    default: {
+      authStatusCookieDomain: "coscene.cn",
+      authStatusCookieName: "coSceneAuthStatus",
+      env: "saas",
+      logo: "coscene",
+      ssoDomain: "sso.coscene.cn",
+      webDomain: "coscene.cn",
+    },
+    "supor.coscene.cn": {
+      authStatusCookieDomain: "coscene.cn",
+      authStatusCookieName: "suporAuthStatus",
+      env: "saas",
+      logo: "supor",
+      ssoDomain: "supor.sso.coscene.cn",
+      webDomain: "supor.coscene.cn",
+    },
+  },
+  FEATURE_FLAGS: {
+    growthbook: true,
+    sentry: true,
+  },
+  FEATURE_FLAG_GROWTHBOOK: {
+    authorize_device: false,
+    custom_workflow: {
+      enabled: false,
+    },
+    cyber_converter: false,
+    diagnosis_rule: false,
+    file_lineage: false,
+    mesh_network_config: {
+      api_url: "",
+      enabled: false,
+    },
+    record_intelligent_analytics: false,
+  },
+  GROWTHBOOK_API_HOST: "https://ff-proxy.coscene.site",
+  GROWTHBOOK_CLIENT_KEY: "sdk-Ycihhhe0GFXizLlf",
+  LAUNCHDARKLY_CLIENT_SIDE_ID: "646c30408ce457125d46eb8f",
+  MESH_API_URL: "https://api.mesh.c5kpjrkfegpq6rvjenh64tbj18.coscene.cn",
+  SENTRY_AUTH_TOKEN: "5d7fd483a3c5412f80bc5ce61fcad735868109e4c221498f97c34c39fac24e12",
+  SENTRY_WEB_DSN: "https://0adc9bf1de394108b5e22679cff97123@sentry.coscene.site/4",
+  VITE_APP_BASE_API_PORT: 443,
+  VITE_APP_BASE_API_URL: "https://api.coscene.cn",
+  VITE_APP_BFF_URL: "https://bff.coscene.cn",
+  VITE_APP_BFF_WS_URL: "wss://bff.coscene.cn",
+  VITE_APP_CR_DOMAIN: "cr.coscene.cn",
+  VITE_APP_PROJECT_ENV: "saas",
+};
+
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-const cosConfig = window.cosConfig ?? {};
+const cosConfig = window.cosConfig ?? defaultCnConfig;
 export const APP_CONFIG = {
   VITE_APP_BASE_API_PORT:
     cosConfig.VITE_APP_BASE_API_PORT ?? process.env.VITE_APP_BASE_API_PORT ?? "443",
