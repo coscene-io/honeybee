4c7844df89d34a13cef8599c825b2c67e1bf53c5
Woodii <33593572+Woodii1998@users.noreply.github.com>
2025-02-12 13:47:20 +0800

    feat: move sentry to shared root (#790)

--- packages/studio-base/src/SharedRoot.tsx
+++ packages/studio-base/src/SharedRoot.tsx
@@ -5,9 +5,11 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import * as Sentry from "@sentry/browser";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
+import Logger from "@foxglove/log";
 import GlobalCss from "@foxglove/studio-base/components/GlobalCss";
 import {
   ISharedRootContext,
@@ -20,6 +22,8 @@ import { ColorSchemeThemeProvider } from "./components/ColorSchemeThemeProvider"
 import CssBaseline from "./components/CssBaseline";
 import ErrorBoundary from "./components/ErrorBoundary";
 import AppConfigurationContext from "./context/AppConfigurationContext";
+
+const log = Logger.getLogger(__filename);
 
 if (APP_CONFIG.POSTHOG.token && APP_CONFIG.POSTHOG.api_host) {
   posthog.init(APP_CONFIG.POSTHOG.token, {
@@ -50,6 +54,30 @@ export function SharedRoot(
     extraProviders,
     extensionLoaders,
   } = props;
+  if (APP_CONFIG.VITE_APP_PROJECT_ENV !== "local" && APP_CONFIG.SENTRY_ENABLED) {
+    log.info("initializing Sentry");
+    Sentry.init({
+      dsn: APP_CONFIG.SENTRY_HONEYBEE_DSN,
+      release: APP_CONFIG.RELEASE_TAG,
+      autoSessionTracking: true,
+      // Remove the default breadbrumbs integration - it does not accurately track breadcrumbs and
+      // creates more noise than benefit.
+      integrations: (integrations) => {
+        return integrations
+          .filter((integration) => integration.name !== "Breadcrumbs")
+          .concat([
+            Sentry.browserTracingIntegration({
+              instrumentNavigation: false,
+            }),
+          ]);
+      },
+      environment: APP_CONFIG.VITE_APP_PROJECT_ENV,
+
+      // We recommend adjusting this value in production, or using tracesSampler
+      // for finer control
+      tracesSampleRate: 0.1,
+    });
+  }
 
   return (
     <AppConfigurationContext.Provider value={appConfiguration}>
