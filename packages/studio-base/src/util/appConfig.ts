0cc2bc03d20226a54938d150fa673ece6ef88bdb
Woodii <1322756558@qq.com>
2025-02-12 18:42:41 +0800

    bifurcation

--- packages/studio-base/src/util/appConfig.ts
+++ packages/studio-base/src/util/appConfig.ts
@@ -5,7 +5,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Language } from "@foxglove/studio-base/i18n";
+import { Language } from "../i18n";
 
 interface DomainConfig {
   env: string;
