947263fafb7e68a162b398df948a61b525879cd9
Woodii <33593572+Woodii1998@users.noreply.github.com>
2025-02-12 13:47:20 +0800

    feat: move sentry to shared root (#790)

--- packages/studio-web/src/index.tsx
+++ packages/studio-web/src/index.tsx
@@ -4,14 +4,12 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-import * as Sentry from "@sentry/browser";
 import { StrictMode, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import Logger from "@foxglove/log";
 import type { CoSceneIDataSourceFactory } from "@foxglove/studio-base";
 import CssBaseline from "@foxglove/studio-base/components/CssBaseline";
-import { APP_CONFIG } from "@foxglove/studio-base/util/appConfig";
 
 import VersionBanner from "./VersionBanner";
 import { canRenderApp } from "./canRenderApp";
@@ -39,31 +37,6 @@ export async function main(getParams: () => Promise<MainParams> = async () => ({
   window.onerror = (...args) => {
     console.error(...args);
   };
-
-  if (APP_CONFIG.VITE_APP_PROJECT_ENV !== "local" && APP_CONFIG.SENTRY_ENABLED) {
-    log.info("initializing Sentry");
-    Sentry.init({
-      dsn: APP_CONFIG.SENTRY_HONEYBEE_DSN,
-      release: APP_CONFIG.RELEASE_TAG,
-      autoSessionTracking: true,
-      // Remove the default breadbrumbs integration - it does not accurately track breadcrumbs and
-      // creates more noise than benefit.
-      integrations: (integrations) => {
-        return integrations
-          .filter((integration) => integration.name !== "Breadcrumbs")
-          .concat([
-            Sentry.browserTracingIntegration({
-              instrumentNavigation: false,
-            }),
-          ]);
-      },
-      environment: APP_CONFIG.VITE_APP_PROJECT_ENV,
-
-      // We recommend adjusting this value in production, or using tracesSampler
-      // for finer control
-      tracesSampleRate: 0.1,
-    });
-  }
 
   const rootEl = document.getElementById("root");
   if (!rootEl) {
