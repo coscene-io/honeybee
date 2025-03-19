694ab1e01e2a8945f0bc79e3254502b584ff56bc
Woodii <1322756558@qq.com>
2025-03-19 18:59:01 +0800

    Committed some changes

--- packages/studio-desktop/src/electronBuilderConfig.js
+++ packages/studio-desktop/src/electronBuilderConfig.js
@@ -22,7 +22,7 @@ function makeElectronBuilderConfig(params) {
       app: params.appPath,
       buildResources: path.join(__dirname, "../resources"),
     },
-    artifactName: "coStudio-v${version}-${os}-${arch}.${ext}",
+    artifactName: "coStudio_${version}-${os}_${arch}.${ext}",
     afterPack: path.resolve(__dirname, "afterPack.ts"),
     icon: path.join(__dirname, "../resources/icon/icon.icns"),
     protocols: [
