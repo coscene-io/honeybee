c5aae521466d49f4c74749079d071ab0d50a3360
Woodii <33593572+Woodii1998@users.noreply.github.com>
2025-04-09 15:51:07 +0800

    fix: update docs link (#839)

--- packages/studio-base/src/util/getDocsLink.ts
+++ packages/studio-base/src/util/getDocsLink.ts
@@ -23,18 +23,18 @@ export function getDocsLink(path?: string): string {
 
 export function getLegalDocsLink(type: "terms" | "privacy" | "security"): string {
   const TERMS_DOC_URL = {
-    en: `${APP_CONFIG.DOC_BASE_URL}legal/terms/en/terms.html`,
-    zh: `${APP_CONFIG.DOC_BASE_URL}legal/terms/zh/terms.html`,
+    en: `${APP_CONFIG.DOC_BASE_URL}/legal/terms/en/terms.html`,
+    zh: `${APP_CONFIG.DOC_BASE_URL}/legal/terms/zh/terms.html`,
   };
 
   const PRIVACY_DOC_URL = {
-    en: `${APP_CONFIG.DOC_BASE_URL}legal/privacy/en/privacy.html`,
-    zh: `${APP_CONFIG.DOC_BASE_URL}legal/privacy/zh/privacy.html`,
+    en: `${APP_CONFIG.DOC_BASE_URL}/legal/privacy/en/privacy.html`,
+    zh: `${APP_CONFIG.DOC_BASE_URL}/legal/privacy/zh/privacy.html`,
   };
 
   const SECURITY_DOC_URL = {
-    en: `${APP_CONFIG.DOC_BASE_URL}security/security-white-paper/en/security-white-paper.html`,
-    zh: `${APP_CONFIG.DOC_BASE_URL}security/security-white-paper/zh/security-white-paper.html`,
+    en: `${APP_CONFIG.DOC_BASE_URL}/security/security-white-paper/en/security-white-paper.html`,
+    zh: `${APP_CONFIG.DOC_BASE_URL}/security/security-white-paper/zh/security-white-paper.html`,
   };
 
   const lang = i18next.language === "zh" ? "zh" : "en";
