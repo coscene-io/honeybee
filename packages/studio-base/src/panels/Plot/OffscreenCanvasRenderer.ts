527b38b9cd386ae46658ca5eeed846dad5e90019
Woodii <33593572+Woodii1998@users.noreply.github.com>
2025-06-04 09:53:30 +0800

    feat: add latest file uploads for mac, windows, and linux in release workflow (#899)

--- packages/studio-base/src/panels/Plot/OffscreenCanvasRenderer.ts
+++ packages/studio-base/src/panels/Plot/OffscreenCanvasRenderer.ts
@@ -5,8 +5,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import * as Comlink from "@coscene-io/comlink";
 import type { Theme } from "@mui/material";
-import * as Comlink from "comlink";
 
 import { ComlinkWrap } from "@foxglove/den/worker";
 import { Immutable } from "@foxglove/studio";
--- packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/WorkerImageDecoder.worker.ts
@@ -5,7 +5,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import * as Comlink from "comlink";
+import * as Comlink from "@coscene-io/comlink";
 
 import Logger from "@foxglove/log";
 import { isLessThan, Time, toMicroSec } from "@foxglove/rostime";
