3ef5263106e9040049b6ce39afcea6d5af9f4e80
Woodii <33593572+Woodii1998@users.noreply.github.com>
2025-06-04 09:53:30 +0800

    feat: add latest file uploads for mac, windows, and linux in release workflow (#899)

--- packages/studio-base/src/panels/Plot/builders/TimestampDatasetsBuilder.ts
+++ packages/studio-base/src/panels/Plot/builders/TimestampDatasetsBuilder.ts
@@ -5,7 +5,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import * as Comlink from "comlink";
+import * as Comlink from "@coscene-io/comlink";
 
 import { ComlinkWrap } from "@foxglove/den/worker";
 import { MessagePath } from "@foxglove/message-path";
--- packages/studio-base/src/players/IterablePlayer/ComlinkTransferIteratorCursor.ts
+++ packages/studio-base/src/players/IterablePlayer/ComlinkTransferIteratorCursor.ts
@@ -4,7 +4,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-import * as Comlink from "comlink";
+import * as Comlink from "@coscene-io/comlink";
 
 import { Time } from "@foxglove/studio";
 
