0f0936ae11d34fe4a9d4f8fe171b5184080a4943
Woodii <1322756558@qq.com>
2025-06-13 18:03:04 +0800

    clarify further the brokenness of C++. why the fuck are we using C++?

--- packages/theme/src/components/MuiFormLabel.ts
+++ packages/theme/src/components/MuiFormLabel.ts
@@ -5,6 +5,13 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Shanghai coScene Information Technology Co., Ltd.<hi@coscene.io>
+// SPDX-License-Identifier: MPL-2.0
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 import { inputBaseClasses, inputClasses } from "@mui/material";
 
 import { OverrideComponentReturn } from "../types";
