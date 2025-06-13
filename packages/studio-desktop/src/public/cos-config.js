980eb72dbdc1c6ae9b7e7f323f33974fa09dd1ea
Woodii <1322756558@qq.com>
2025-06-13 17:05:58 +0800

    feat: support messageTime

--- packages/studio-desktop/src/public/cos-config.js
+++ packages/studio-desktop/src/public/cos-config.js
@@ -5,4 +5,71 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-window.cosConfig = {};
+window.cosConfig = {
+  COORDINATOR_URL: "https://coordinator.coscene.cn",
+  COSTUDIO_DOWNLOAD_URL: "https://download.coscene.cn/coStudio/packages",
+  CS_HONEYBEE_BASE_URL: "https://viz.coscene.cn",
+  DOC_BASE_URL: "https://docs.coscene.cn",
+  DOMAIN_CONFIG: {
+    "agibot.coscene.cn": {
+      authStatusCookieDomain: "coscene.cn",
+      authStatusCookieName: "agibotAuthStatus",
+      env: "saas",
+      logo: "agibot",
+      ssoDomain: "agibot.sso.coscene.cn",
+      webDomain: "agibot.coscene.cn",
+    },
+    default: {
+      authStatusCookieDomain: "coscene.cn",
+      authStatusCookieName: "coSceneAuthStatus",
+      env: "saas",
+      logo: "coscene",
+      ssoDomain: "sso.coscene.cn",
+      webDomain: "coscene.cn",
+    },
+    "supor.coscene.cn": {
+      authStatusCookieDomain: "coscene.cn",
+      authStatusCookieName: "suporAuthStatus",
+      env: "saas",
+      logo: "supor",
+      ssoDomain: "supor.sso.coscene.cn",
+      webDomain: "supor.coscene.cn",
+    },
+  },
+  FEATURE_FLAGS: {
+    growthbook: true,
+    sentry: true,
+  },
+  FEATURE_FLAG_GROWTHBOOK: {
+    authorize_device: false,
+    cyber_converter: false,
+    diagnosis_rule: false,
+    mesh_network_config: {
+      api_url: "",
+      enabled: false,
+    },
+    record_intelligent_analytics: false,
+  },
+  GROWTHBOOK_API_HOST: "https://ff-proxy.coscene.site",
+  GROWTHBOOK_CLIENT_KEY: "sdk-Ycihhhe0GFXizLlf",
+  INVITE_USER_METHOD: "phone",
+  LANGUAGE: {
+    default: "zh",
+    options: ["en", "zh"],
+  },
+  MATRIX_HOST: "matrix.coscene.cn",
+  MESH_API_URL: "https://api.mesh.c5kpjrkfegpq6rvjenh64tbj18.coscene.cn",
+  OFFICIAL_WEB_URL: "https://www.coscene.cn",
+  POSTHOG: {
+    api_host: "https://posthog.coscene.site",
+    token: "phc_Toi3ks6md0takiQhnpFeB7bxlVbrWsea1MZB3NSpNvU",
+  },
+  SENTRY_AUTH_TOKEN: "5d7fd483a3c5412f80bc5ce61fcad735868109e4c221498f97c34c39fac24e12",
+  SENTRY_WEB_DSN: "https://0adc9bf1de394108b5e22679cff97123@sentry.coscene.site/4",
+  VITE_APP_BASE_API_PORT: 443,
+  VITE_APP_BASE_API_URL: "https://api.coscene.cn",
+  VITE_APP_BFF_URL: "https://bff.coscene.cn",
+  VITE_APP_BFF_WS_URL: "wss://bff.coscene.cn",
+  VITE_APP_CR_DOMAIN: "cr.coscene.cn",
+  VITE_APP_PROJECT_ENV: "saas",
+};
