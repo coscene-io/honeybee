152d1addbec1209f8fa7c9db70d23fdacd3eea47
zychen31 <zhiyong.chen@coscene.io>
2025-08-08 17:40:27 +0800

    feat: update backgroundColor

--- packages/studio-base/src/components/Tasks/TaskDetailDrawer/components/LinkedRecordsTable/components/RecordTable.tsx
+++ packages/studio-base/src/components/Tasks/TaskDetailDrawer/components/LinkedRecordsTable/components/RecordTable.tsx
@@ -102,7 +102,7 @@ const useStyles = makeStyles()((theme) => ({
     },
     backgroundColor: theme.palette.background.paper,
     "&:hover": {
-      backgroundColor: alpha(theme.palette.background.paper, 0.8),
+      backgroundColor: alpha(theme.palette.grey[400], 0.8),
     },
   },
   deviceId: {
