06d86a82bed2e304f621f2dcc0cbd74d0bd3340e
Woodii <1322756558@qq.com>
2025-08-30 19:42:00 +0800

    Please forgive me

--- packages/studio-base/src/components/MessagePathSyntax/useMessageDataItem.ts
+++ packages/studio-base/src/components/MessagePathSyntax/useMessageDataItem.ts
@@ -26,7 +26,27 @@ import {
 } from "./useCachedGetMessagePathDataItems";
 
 type Options = {
-  historySize: number;
+  // messagePipeline 会分发每个渲染帧时间范围内的所有消息，比如如果现在设置的是一秒 60 帧，
+  // 那么 messagePipeline 就会每次分发 1/60 秒内的所有消息,
+  // 正常情况下只需要在意并且渲染离当前时间最接近的消息，也就是数组内最后一条消息
+  // 所以默认情况下 historySize 设置为 1
+  // 但是有些情况下需要返回多帧消息，比如和上一帧数据进行对比时，就需要返回最近的两帧
+  // 这时候就需要把 historySize 设置为 2
+  // 但是还有更极端的情况，比如我们记住每一帧的时间戳，方便用户返回上一帧，这时候就要返回一个渲染帧内所有的消息
+  // 这时候就需要把 historySize 设置为 "all"
+
+  // messagePipeline will dispatch all messages within a render frame, for example, if the current
+  // setting is 60 frames per second, messagePipeline will dispatch all messages within 1/60 seconds.
+  // Normally, you only need to pay attention to and render the message closest to the current time,
+  // which is the last message in the array.
+  // so the default historySize is 1.
+  // But in some cases, you need to return multiple frames of messages, for example, when comparing
+  // with the previous frame, you need to return the last two frames.
+  // In this case, you need to set historySize to 2.
+  // But in some cases, you need to return all messages within a render frame, for example, when
+  // remembering the timestamp of each frame, you need to return all messages within a render frame.
+  // In this case, you need to set historySize to "all".
+  historySize: number | "all";
 
   onRestore?: () => void;
 };
@@ -50,7 +70,7 @@ type ReducedValue = {
  * The `historySize` option configures how many matching messages to keep. The default is 1.
  */
 export function useMessageDataItem(path: string, options?: Options): ReducedValue["matches"] {
-  const { historySize = 1 } = options ?? {};
+  const { historySize = 1, onRestore } = options ?? {};
   const topics: SubscribePayload[] = useMemo(() => {
     const payload = subscribePayloadFromMessagePath(path, "partial");
     if (payload) {
@@ -67,11 +87,13 @@ export function useMessageDataItem(path: string, options?: Options): ReducedValu
         return prevValue;
       }
 
+      const realHistorySize = historySize === "all" ? messageEvents.length : historySize;
+
       const newMatches: MessageAndData[] = [];
 
       // Iterate backwards since our default history size is 1 and we might not need to visit all messages
       // This does mean we need to flip newMatches around since we want to store older items first
-      for (let i = messageEvents.length - 1; i >= 0 && newMatches.length < historySize; --i) {
+      for (let i = messageEvents.length - 1; i >= 0 && newMatches.length < realHistorySize; --i) {
         const messageEvent = messageEvents[i]!;
         const queriedData = cachedGetMessagePathDataItems(path, messageEvent);
         if (queriedData && queriedData.length > 0) {
@@ -92,7 +114,7 @@ export function useMessageDataItem(path: string, options?: Options): ReducedValu
 
       const prevMatches = prevValue.matches;
       return {
-        matches: prevMatches.concat(reversed).slice(-historySize),
+        matches: prevMatches.concat(reversed).slice(-realHistorySize),
         messageEvents,
         path,
       };
@@ -102,7 +124,7 @@ export function useMessageDataItem(path: string, options?: Options): ReducedValu
 
   const restore = useCallback(
     (prevValue?: ReducedValue): ReducedValue => {
-      options?.onRestore?.();
+      onRestore?.();
 
       if (!prevValue) {
         return {
@@ -121,11 +143,12 @@ export function useMessageDataItem(path: string, options?: Options): ReducedValu
         }
       }
 
+      const realHistorySize = historySize === "all" ? newMatches.length : historySize;
       // Return a new message set if we have matching messages or this is a different path
       // than the path used to fetch the previous set of messages.
       if (newMatches.length > 0 || path !== prevValue.path) {
         return {
-          matches: newMatches.slice(-historySize),
+          matches: newMatches.slice(-realHistorySize),
           messageEvents: prevValue.messageEvents,
           path,
         };
