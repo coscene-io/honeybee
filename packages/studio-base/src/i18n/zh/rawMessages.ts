0d2d7d7054456df3aaf2f42cf72ed1df748ba210
Woodii <1322756558@qq.com>
2025-09-02 13:50:50 +0800

    feat: add internationalization support for RawMessages and Table panels with translation integration

---
+++ packages/studio-base/src/i18n/zh/rawMessages.ts
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Shanghai coScene Information Technology Co., Ltd.<hi@coscene.io>
+// SPDX-License-Identifier: MPL-2.0
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import type { TypeOptions } from "i18next";
+
+export const rawMessages: Partial<TypeOptions["resources"]["rawMessages"]> = {
+  toggleDiff: "切换差异",
+  expandAll: "全部展开",
+  collapseAll: "全部折叠",
+  previousFrame: "上一帧",
+  nextFrame: "下一帧",
+  diffMethod: "对比方式",
+  previousMessage: "上一条消息",
+  custom: "自定义",
+};
+
---
+++ packages/studio-base/src/i18n/zh/table.ts
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Shanghai coScene Information Technology Co., Ltd.<hi@coscene.io>
+// SPDX-License-Identifier: MPL-2.0
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import type { TypeOptions } from "i18next";
+
+export const table: Partial<TypeOptions["resources"]["table"]> = {
+  previousFrame: "上一帧（↑）",
+  nextFrame: "下一帧（↓）",
+  noTopicSelected: "未选择话题",
+  waitingForNextMessage: "等待下一条消息…",
+};
+
