d74cc028e8fed0b330bc1e131fe61457e3d9c488
Qi He <heqi37@gmail.com>
2025-09-02 20:01:12 +0800

    rm onRecommendedToProjectLayout

--- packages/studio-base/src/components/AppBar/CoSceneLayoutButton/index.tsx
+++ packages/studio-base/src/components/AppBar/CoSceneLayoutButton/index.tsx
@@ -517,51 +517,6 @@ export function CoSceneLayoutButton(): React.JSX.Element {
     void analytics.logEvent(AppEvent.LAYOUT_CREATE);
   }, [promptForUnsavedChanges, layoutManager, onSelectLayout, analytics]);
 
-  // const onRecommendedToProjectLayout = useCallbackWithToast(
-  //   async (item: Layout) => {
-  //     todo: delete
-  //     const currentProjectId = externalInitConfig?.projectId;
-  //     const currentRecommendedLayouts = layouts.value?.shared
-  //       .filter((layout) => layout.isProjectRecommended)
-  //       .map((layout) => layout.id);
-  //     if (currentRecommendedLayouts != undefined && currentProjectId != undefined) {
-  //       if (item.isProjectRecommended) {
-  //         const nextRecommendedLayouts = currentRecommendedLayouts.filter((id) => id !== item.id);
-  //         await consoleApi.setProjectRecommendedLayouts(nextRecommendedLayouts, currentProjectId);
-  //       } else {
-  //         const nextRecommendedLayouts = [...currentRecommendedLayouts, item.id];
-  //         await consoleApi.setProjectRecommendedLayouts(nextRecommendedLayouts, currentProjectId);
-  //       }
-  //       await layoutManager.updateLayout({ id: item.id, displayName: item.displayName });
-  //     }
-  //   },
-  //   [],
-  //   [externalInitConfig?.projectId, consoleApi, layoutManager, layouts.value?.shared],
-  // );
-
-  // const onCopyToRecordDefaultLayout = useCallbackWithToast(
-  //   async (item: Layout) => {
-  //     const displayName = await prompt({
-  //       title: t("copyToRecordDefaultLayoutTitle"),
-  //       subText: t("copyToRecordDefaultLayoutDesc"),
-  //       initialValue: item.displayName,
-  //       label: t("layoutName"),
-  //     });
-  //     if (displayName != undefined) {
-  //       const newLayout = await layoutManager.saveNewLayout({
-  //         folder: item.folder,
-  //         displayName,
-  //         data: item.working?.data ?? item.baseline.data,
-  //         permission: "ORG_WRITE",
-  //         isRecordDefaultLayout: true,
-  //       });
-  //       void analytics.logEvent(AppEvent.LAYOUT_SHARE, { permission: item.permission });
-  //       await onSelectLayout(newLayout);
-  //     }
-  //   },
-  //   [analytics, t, layoutManager, onSelectLayout, prompt],
-  // );
-
   const appBarMenuItems = [
     {
       type: "item",
