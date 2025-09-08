272555bc2135ee40d2bd3356d5ec0f1383659eca
Qi He <heqi37@gmail.com>
2025-09-08 21:00:33 +0800

    clean up

--- packages/studio-base/src/context/Workspace/useWorkspaceActions.ts
+++ packages/studio-base/src/context/Workspace/useWorkspaceActions.ts
@@ -5,14 +5,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-// SPDX-FileCopyrightText: Copyright (C) 2023-2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
-// SPDX-License-Identifier: MPL-2.0
-
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-
 import { Draft, produce } from "immer";
 import * as _ from "lodash-es";
 import { Dispatch, SetStateAction, useCallback, useMemo } from "react";
