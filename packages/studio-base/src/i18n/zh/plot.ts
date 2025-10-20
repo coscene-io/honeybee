e0e902acce1c5fc529ad69492156455e8411bf8c
Woodii <33593572+Woodii1998@users.noreply.github.com>
2025-10-20 10:32:11 +0800

    feat: plot series support collapsed all (#1099)

--- packages/studio-base/src/i18n/zh/plot.ts
+++ packages/studio-base/src/i18n/zh/plot.ts
@@ -50,4 +50,6 @@ export const plot: Partial<TypeOptions["resources"]["plot"]> = {
   enableAllSeries: "启用所有",
   disableAllSeries: "禁用所有",
   toggleAllSeries: "切换全部",
+  collapseAllSeries: "全部收起",
+  expandAllSeries: "全部展开",
 };
