2f781eff08c5dbc8fbfec7502df4d2c154b8646e
Qi He <heqi37@gmail.com>
2025-11-03 14:37:08 +0800

    feat: select project on topbar (#1136)

---
+++ packages/studio-base/src/components/AppBar/AppBarProject.tsx
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Shanghai coScene Information Technology Co., Ltd.<hi@coscene.io>
+// SPDX-License-Identifier: MPL-2.0
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { ProjectSelector } from "@foxglove/studio-base/components/CoSceneLayout/createLayout/ProjectSelector";
+import { useSetExternalInitConfig } from "@foxglove/studio-base/components/CoreDataSyncAdapter";
+import { useConsoleApi } from "@foxglove/studio-base/context/CoSceneConsoleApiContext";
+import { CoreDataStore, useCoreData } from "@foxglove/studio-base/context/CoreDataContext";
+import { usePlayerSelection } from "@foxglove/studio-base/context/PlayerSelectionContext";
+
+const selectExternalInitConfig = (state: CoreDataStore) => state.externalInitConfig;
+
+export function AppBarProject(): React.JSX.Element {
+  const consoleApi = useConsoleApi();
+  const { selectSource, selectedSource } = usePlayerSelection();
+
+  const externalInitConfig = useCoreData(selectExternalInitConfig);
+  const setExternalInitConfig = useSetExternalInitConfig();
+
+  const projectName =
+    externalInitConfig?.warehouseId && externalInitConfig.projectId
+      ? `warehouses/${externalInitConfig.warehouseId}/projects/${externalInitConfig.projectId}`
+      : "";
+
+  return (
+    <ProjectSelector
+      showLabel={false}
+      value={projectName}
+      onChange={(value) => {
+        const array = value.split("/");
+        const newExternalInitConfig = {
+          warehouseId: array[1],
+          projectId: array[3],
+        };
+        if (selectedSource?.type !== "file") {
+          selectSource(undefined);
+        }
+
+        void setExternalInitConfig(newExternalInitConfig);
+        void consoleApi.setApiBaseInfo(newExternalInitConfig);
+      }}
+    />
+  );
+
+  return <></>;
+}
