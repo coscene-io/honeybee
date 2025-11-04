2da21f0560f3fb06a8a8532e41256f2636040f77
Qi He <heqi37@gmail.com>
2025-11-04 17:39:18 +0800

    lint

--- packages/studio-desktop/src/main/StudioWindow.ts
+++ packages/studio-desktop/src/main/StudioWindow.ts
@@ -310,15 +310,15 @@ function buildMenu(browserWindow: BrowserWindow): Menu {
       { role: "paste", label: t("desktopWindow:paste") },
       ...(isMac
         ? [
-          { role: "pasteAndMatchStyle", label: t("desktopWindow:pasteAndMatchStyle") } as const,
-          { role: "delete", label: t("desktopWindow:delete") } as const,
-          { role: "selectAll", label: t("desktopWindow:selectAll") } as const,
-        ]
+            { role: "pasteAndMatchStyle", label: t("desktopWindow:pasteAndMatchStyle") } as const,
+            { role: "delete", label: t("desktopWindow:delete") } as const,
+            { role: "selectAll", label: t("desktopWindow:selectAll") } as const,
+          ]
         : [
-          { role: "delete", label: t("desktopWindow:delete") } as const,
-          { type: "separator" } as const,
-          { role: "selectAll", label: t("desktopWindow:selectAll") } as const,
-        ]),
+            { role: "delete", label: t("desktopWindow:delete") } as const,
+            { type: "separator" } as const,
+            { role: "selectAll", label: t("desktopWindow:selectAll") } as const,
+          ]),
     ],
   });
 
@@ -331,15 +331,15 @@ function buildMenu(browserWindow: BrowserWindow): Menu {
       workers.length === 0
         ? [{ label: t("desktopWindow:noSharedWorkers"), enabled: false }]
         : workers.map(
-          (worker) =>
-            new MenuItem({
-              label: worker.url,
-              click() {
-                browserWindow.webContents.closeDevTools();
-                browserWindow.webContents.inspectSharedWorkerById(worker.id);
-              },
-            }),
-        ),
+            (worker) =>
+              new MenuItem({
+                label: worker.url,
+                click() {
+                  browserWindow.webContents.closeDevTools();
+                  browserWindow.webContents.inspectSharedWorkerById(worker.id);
+                },
+              }),
+          ),
     ).popup();
   };
 
