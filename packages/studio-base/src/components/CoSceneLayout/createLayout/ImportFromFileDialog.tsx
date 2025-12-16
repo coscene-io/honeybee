183eeed909da1e52abf3c622fb393fda4e026b92
Qi He <heqi37@gmail.com>
2025-12-16 10:35:59 +0800

    import dialog

--- packages/studio-base/src/components/CoSceneLayout/createLayout/ImportFromFileDialog.tsx
+++ packages/studio-base/src/components/CoSceneLayout/createLayout/ImportFromFileDialog.tsx
@@ -5,6 +5,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { JsonObject } from "@bufbuild/protobuf";
 import {
   Dialog,
   DialogTitle,
@@ -30,6 +31,7 @@ import {
   CreateLayoutParams,
   CreateLayoutForm,
 } from "@foxglove/studio-base/services/CoSceneILayoutManager";
+import { replaceNullWithUndefined } from "@foxglove/studio-base/util/coscene";
 
 import { SelectFolder } from "./SelectFolder";
 
@@ -122,7 +124,7 @@ export function ImportFromFileDialog({
       return;
     }
 
-    const data = parsedState as LayoutData;
+    const data = replaceNullWithUndefined(parsedState as JsonObject) as LayoutData;
 
     form.setValue("selectedFile", file.name);
     form.setValue("name", layoutName);
