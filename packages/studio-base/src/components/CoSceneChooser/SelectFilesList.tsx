f256de05ffe68b25e8fed3032e14adc7bc0b1668
Qi He <heqi37@gmail.com>
2025-12-17 15:18:46 +0800

    remove select repeat file

--- packages/studio-base/src/components/CoSceneChooser/SelectFilesList.tsx
+++ packages/studio-base/src/components/CoSceneChooser/SelectFilesList.tsx
@@ -7,9 +7,8 @@
 
 import { File } from "@coscene-io/cosceneapis-es-v2/coscene/dataplatform/v1alpha3/resources/file_pb";
 import FolderIcon from "@mui/icons-material/Folder";
-import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
-import { List, ListItem, ListItemButton, Checkbox, ListItemText, Tooltip } from "@mui/material";
+import { List, ListItem, ListItemButton, Checkbox, ListItemText } from "@mui/material";
 import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -71,15 +70,12 @@ export function SelectFilesList({
       const supportedImport = checkFileSupportedFunc(fileItem);
 
       const isSelected = selectedFiles.some((f) => f.file.name === fileItem.name);
-      const repeatFile = selectedFiles.find(
-        (f) => f.file.sha256 === fileItem.sha256 && f.file.name !== fileItem.name,
-      );
       const fileName = fileItem.filename.split("/").pop() ?? fileItem.filename;
 
       return (
         <ListItem key={fileItem.name} disablePadding>
           <ListItemButton
-            disabled={!supportedImport || repeatFile != undefined}
+            disabled={!supportedImport}
             onClick={() => {
               onFileToggle(fileItem);
             }}
@@ -105,22 +101,12 @@ export function SelectFilesList({
                 primary={fileName}
                 secondary={supportedImport ? undefined : "不支持的文件格式"}
               />
-              {repeatFile && (
-                <Tooltip
-                  title={t("duplicateFile", {
-                    ns: "cosPlaylist",
-                    filename: repeatFile.file.name,
-                  })}
-                >
-                  <HelpOutlineIcon fontSize="small" color="error" />
-                </Tooltip>
-              )}
             </Stack>
           </ListItemButton>
         </ListItem>
       );
     },
-    [selectedFiles, onFileToggle, checkFileSupportedFunc, t],
+    [selectedFiles, onFileToggle, checkFileSupportedFunc],
   );
 
   return (
