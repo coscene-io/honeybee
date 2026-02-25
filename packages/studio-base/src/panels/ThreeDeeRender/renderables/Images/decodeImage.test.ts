77a5cb0092e9ebfdd3d79c0cbee7b08be0d1cfb7
Woodii <1322756558@qq.com>
2026-02-25 19:14:13 +0800

    fix: decodeImage

--- packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/decodeImage.test.ts
+++ packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/decodeImage.test.ts
@@ -1,11 +1,11 @@
+/** @jest-environment jsdom */
+
 // SPDX-FileCopyrightText: Copyright (C) 2022-2024 Shanghai coScene Information Technology Co., Ltd.<hi@coscene.io>
 // SPDX-License-Identifier: MPL-2.0
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-/** @jest-environment jsdom */
 
 import { H264 } from "@foxglove/den/video";
 
